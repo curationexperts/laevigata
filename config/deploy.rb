aaecc8f93a3693c659af84d43b63f9dfa70e57a4
Bess Sadler <bess@curationexperts.com>
2017-04-06 13:10:54 -0700

    Initial capification

---
+++ config/deploy.rb
@@ -0,0 +1,65 @@
+# config valid only for current version of Capistrano
+lock "3.8.0"
+
+set :application, "laevigata"
+set :repo_url, "https://github.com/curationexperts/laevigata.git"
+set :deploy_to, '/opt/laevigata'
+
+set :log_level, :debug
+set :bundle_flags, '--deployment'
+set :bundle_env_variables, nokogiri_use_system_libraries: 1
+
+set :keep_releases, 5
+set :passenger_restart_with_touch, true
+set :assets_prefix, "#{shared_path}/public/assets"
+
+SSHKit.config.command_map[:rake] = 'bundle exec rake'
+
+# Default branch is :master
+set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
+
+set :linked_dirs, %w[
+  tmp/pids
+  tmp/cache
+  tmp/sockets
+  public/assets
+]
+
+set :linked_files, %w[
+  config/blacklight.yml
+  config/database.yml
+  config/fedora.yml
+  config/redis.yml
+  config/resque-pool.yml
+  config/role_map.yml
+  config/secrets.yml
+  config/solr.yml
+]
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
