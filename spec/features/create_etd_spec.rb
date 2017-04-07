15f91266e274e3b2fc833f1fb19a57af896bb9df
Bess Sadler <bess@curationexperts.com>
2017-04-06 17:40:51 -0700

    Automated test for submitting an MS Word ETD

--- spec/features/create_etd_spec.rb
+++ spec/features/create_etd_spec.rb
@@ -16,11 +16,22 @@ RSpec.feature 'Create a Etd' do
       login_as user
     end
 
-    scenario do
-      visit new_curation_concerns_etd_path
-      fill_in 'Title', with: 'Test Etd'
-      click_button 'Create Etd'
-      expect(page).to have_content 'Test Etd'
+    scenario "Submit a basic MS Word Thesis" do # rubocop: disable RSpec/ExampleLength
+      visit("/concern/etds/new")
+      fill_in 'Title', with: 'China and its Minority Population'
+      fill_in 'Creator', with: 'Eun, Dongwon'
+      fill_in 'Keyword', with: 'China'
+      select('All rights reserved', from: 'Rights')
+      choose('open')
+      check('agreement')
+      click_on('Files')
+      attach_file('files[]', "#{fixture_path}/emory_7tjfb-FILE")
+      click_on('Save')
+      expect(page).to have_content 'Your files are being processed'
+      expect(page).to have_content 'deposited'
+      expect(page).to have_content 'China and its Minority Population'
+      # Department is not required, by default it is hidden as an additional field
+      # click_link("Additional fields")
     end
   end
 end
