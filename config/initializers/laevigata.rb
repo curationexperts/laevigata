09f266a0db12b81da1a9d7dd37b6f0a6c85a9b3a
Bess Sadler <bess@curationexperts.com>
2017-05-04 17:36:44 -0400

    Fix travis build Run db:setup and db:migrate before CI tests Fix rubocop offense Only run workflow initializer in dev and prod environments

--- config/initializers/laevigata.rb
+++ config/initializers/laevigata.rb
@@ -1,8 +1,12 @@
 require 'workflow_setup'
-Rails.application.config.after_initialize do
-  w = WorkflowSetup.new
-  w.make_mediated_deposit_admin_set("School One")
-  w.make_mediated_deposit_admin_set("School Two")
-  w.make_mediated_deposit_admin_set("School Three")
-  w.make_mediated_deposit_admin_set("School Four")
+if Rails.env.development? || Rails.env.production?
+  if ActiveRecord::Base.connection.data_source_exists? 'users'
+    Rails.application.config.after_initialize do
+      w = WorkflowSetup.new
+      w.make_mediated_deposit_admin_set("School One")
+      w.make_mediated_deposit_admin_set("School Two")
+      w.make_mediated_deposit_admin_set("School Three")
+      w.make_mediated_deposit_admin_set("School Four")
+    end
+  end
 end
