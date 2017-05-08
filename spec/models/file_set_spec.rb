058f567b1715a184cafe920d605ba7763acaae83
Collin Brittle <cnbrittle@gmail.com>
2017-05-08 15:10:35 +0000

    Adds primary? and supplementary? methods to FileSets

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe FileSet do
-  it "has tests" do
-    skip "Add your tests here"
+  describe 'primary' do
+    subject { described_class.new }
+
+    context 'with a new FileSet' do
+      its(:primary?) { is_expected.not_to be_truthy }
+      its(:supplementary?) { is_expected.to be_truthy }
+    end
+
+    context 'with a FileSet marked primary' do
+      # TODO
+      its(:primary?) { is_expected.to be_truthy }
+      its(:supplementary?) { is_expected.not_to be_truthy }
+    end
   end
 end
