d1363d011593f54234f456efd0c526476db1fce7
Alex Zotov <alexandr.zotov@emory.edu>
2017-06-20 09:27:23 -0400

    adding tests for models and forms

--- spec/models/etd_spec.rb
+++ spec/models/etd_spec.rb
@@ -156,6 +156,182 @@ RSpec.describe Etd do
     end
   end
 
+  describe "#primary_title" do
+    subject { described_class.new }
+    let(:primary_title) { "Cool Etd" }
+    context "with a new ETD" do
+      its(:primary_title) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a primary title defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.primary_title = [primary_title]
+        end
+      end
+      its(:primary_title) { is_expected.to eq([primary_title]) }
+    end
+  end
+
+  describe "#supplemental_title" do
+    subject { described_class.new }
+    let(:supplemental_title) { "Cool Etd" }
+    context "with a new ETD" do
+      its(:supplemental_title) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a supplemental title defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.supplemental_title = [supplemental_title]
+        end
+      end
+      its(:supplemental_title) { is_expected.to eq([supplemental_title]) }
+    end
+  end
+
+  describe "#language" do
+    subject { described_class.new }
+    let(:language) { "English" }
+    context "with a new ETD" do
+      its(:language) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a language defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.language = [language]
+        end
+      end
+      its(:language) { is_expected.to eq([language]) }
+    end
+  end
+
+  describe "#abstract" do
+    subject { described_class.new }
+    let(:abstract) { "This an amazing abstract" }
+    context "with a new ETD" do
+      its(:abstract) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a abstract defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.abstract = [abstract]
+        end
+      end
+      its(:abstract) { is_expected.to eq([abstract]) }
+    end
+  end
+
+  describe "#table_of_contents" do
+    subject { described_class.new }
+    let(:table_of_contents) { "This is my table of contents" }
+    context "with a new ETD" do
+      its(:table_of_contents) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a table of contents defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.table_of_contents = [table_of_contents]
+        end
+      end
+      its(:table_of_contents) { is_expected.to eq([table_of_contents]) }
+    end
+  end
+
+  describe "#keyword" do
+    subject { described_class.new }
+    let(:keyword) { "astrology" }
+    context "with a new ETD" do
+      its(:keyword) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a keyword defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.keyword = [keyword]
+        end
+      end
+      its(:keyword) { is_expected.to eq([keyword]) }
+    end
+  end
+
+  describe "#file_format" do
+    subject { described_class.new }
+    let(:file_format) { "application/pdf" }
+    context "with a new ETD" do
+      its(:file_format) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a keyword defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.file_format = [file_format]
+        end
+      end
+      its(:file_format) { is_expected.to eq([file_format]) }
+    end
+  end
+
+  describe "#identifier" do
+    subject { described_class.new }
+    let(:identifier) { "ID" }
+    context "with a new ETD" do
+      its(:identifier) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a identifier defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.identifier = [identifier]
+        end
+      end
+      its(:identifier) { is_expected.to eq([identifier]) }
+    end
+  end
+
+  describe "#author" do
+    subject { described_class.new }
+    let(:author) { "ID" }
+    context "with a new ETD" do
+      its(:author) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a author defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.author = [author]
+        end
+      end
+      its(:author) { is_expected.to eq([author]) }
+    end
+  end
+
+  describe "#description" do
+    subject { described_class.new }
+    let(:description) { "ID" }
+    context "with a new ETD" do
+      its(:description) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a description defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.description = [description]
+        end
+      end
+      its(:description) { is_expected.to eq([description]) }
+    end
+  end
+
+  describe "#file_type" do
+    subject { described_class.new }
+    let(:file_type) { "Sound" }
+    context "with a new ETD" do
+      its(:file_type) { is_expected.to be_empty }
+    end
+    context "with an existing ETD that has a file type defined" do
+      subject do
+        described_class.create.tap do |etd|
+          etd.file_type = [file_type]
+        end
+      end
+      its(:file_type) { is_expected.to eq([file_type]) }
+    end
+  end
+
   describe "#research_field and #research_field_id" do
     subject(:etd) { described_class.new }
     let(:research_field) { ['Toxicology'] }
---
+++ spec/services/hyrax/filetype_service_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Hyrax::FiletypeService do
+  before do
+    # Configure QA to use fixtures
+    qa_fixtures = { local_path: File.expand_path('config/authorities') }
+    allow(Qa::Authorities::Local).to receive(:config).and_return(qa_fixtures)
+  end
+
+  let(:service) { described_class.new }
+
+  describe "#select_all_options" do
+    it "has a select list" do
+      expect(service.select_all_options).to include(["Sound", "Sound"])
+    end
+  end
+end
