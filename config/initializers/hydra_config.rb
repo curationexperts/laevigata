e0c1062b1702ee39be2a190b8ba55b8390bd7c75
Bess Sadler <bess@curationexperts.com>
2017-06-21 09:03:09 -0400

    Updating dependencies and setting now-required config field

--- config/initializers/hydra_config.rb
+++ config/initializers/hydra_config.rb
@@ -18,5 +18,6 @@ Hydra.configure do |config|
   #
   #
   # Specify the user model
-  # config.user_model = 'User'
+  config.user_model = 'User'
+  config.user_key_field = Devise.authentication_keys.first
 end
