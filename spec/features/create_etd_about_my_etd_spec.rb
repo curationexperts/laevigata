89757f7a587178464bf818c89b14279e3cdc940d
Jennifer Lindner <jenlindner@gmail.com>
2017-07-13 10:48:05 -0500

    review my etd preview and submit

--- spec/features/create_etd_about_my_etd_spec.rb
+++ spec/features/create_etd_about_my_etd_spec.rb
@@ -4,6 +4,16 @@ require 'rails_helper'
 
 include Warden::Test::Helpers
 
+def tinymce_fill_in(id, val)
+  # wait until the TinyMCE editor instance is ready. This is required for cases
+  # where the editor is loaded via XHR.
+  # sleep 0.5 until
+  page.evaluate_script("tinyMCE.get('#{id}') !== null")
+
+  js = "tinyMCE.get('#{id}').setContent('#{val}')"
+  page.execute_script(js)
+end
+
 RSpec.feature 'Create an Etd: About My Etd' do
   let(:user) { create :user }
 
@@ -31,20 +41,16 @@ RSpec.feature 'Create an Etd: About My Etd' do
     end
 
     scenario "can save 'about my etd'", js: true do
-      pending
       click_on("About My ETD")
       fill_in 'Title', with: 'Middlemarch'
       select("French", from: "Language")
-      fill_in 'Abstract', with: "Literature from the US"
-      fill_in 'Table of contents', with: "Chapter One"
+      tinymce_fill_in('etd_abstract', 'Literature from the US')
+      tinymce_fill_in('etd_table_of_contents', 'Chapter One')
       select 'Aeronomy', from: 'Research Field'
       fill_in 'Keyword', with: "Courtship"
-      choose 'etd_copyright_question_one_true'
-      choose 'etd_copyright_question_two_false'
-      choose 'etd_copyright_question_three_true'
-
       click_on('about_my_etd_data')
       expect(page).to have_content 'Successfully saved About My Etd'
+      expect(page).to have_css 'li#required-my-etd.complete'
     end
 
     scenario "manages limit of 3 research fields", js: true do
