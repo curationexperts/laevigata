2367059a5a70296b2c7af0e6e22f2ad5579fa124
Alex Zotov <alexandr.zotov@emory.edu>
2017-07-19 16:33:26 -0400

    required committee members

--- app/assets/javascripts/dependent_dropdowns.js
+++ app/assets/javascripts/dependent_dropdowns.js
@@ -17,8 +17,16 @@ observer.attr('disabled', true);
 observed.on('change', function () {
 observer.empty().append(prompt);
 if (observed.val()) {
-// console.log(observer_dom_id)
-// console.log(observed.val())
+
+if($('#etd_school option:selected').index()==4){
+	
+	$('#help-members').remove();
+	$("#etd_committee_members_0_name").prop('required',false);
+}else{
+	$('.etd_committee_members_0_name label').append("<p class='help-block' id='help-members'>At least one committee member is required</p>")
+	$("#etd_committee_members_0_name").prop('required',true);
+	
+}
 var res = observed.val().split(" ")[0].toLowerCase() + "_programs";
 url = url_mask.replace(regexp, res);
 
@@ -28,6 +36,9 @@ observer.append($('<option>').attr('value', object.id).text(object.label));
 observer.attr('disabled', false);
 });
 });
+}else{
+	$('#help-members').remove();
+	$("#etd_committee_members_0_name").prop('required',false);
 }
 });
 });
