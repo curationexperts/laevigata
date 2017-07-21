77d5c51d573598b95c52237d6e85c2cb3f6af03f
Jennifer Lindner <jenlindner@gmail.com>
2017-07-21 16:12:37 -0500

    helper method for skipping specs in ci

--- spec/support/integration_helpers.rb
+++ spec/support/integration_helpers.rb
@@ -2,3 +2,7 @@ def show_page
   save_page Rails.root.join('public', 'capybara.html')
   `launchy http://localhost:3000/capybara.html`
 end
+
+def continuous_integration?
+  ENV.fetch("TRAVIS", false)
+end
