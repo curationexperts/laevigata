8c1d7584520840c6cd0de2f15aa49faa28ff8655
Jennifer Lindner <jenlindner@gmail.com>
2017-07-31 16:36:42 -0500

    changing Share Your Work link to Submit My ETD

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,7 +5,9 @@ RSpec.feature 'Login a User' do
   context 'a logged out user' do
     scenario 'Submit an ETD' do
       visit(root_url)
-      click_link('Share Your Work')
+
+      expect(page).to have_link('Submit My ETD')
+      click_link('Submit My ETD')
       expect(page).to have_xpath('//h2', text: 'Log in')
       expect(page).to have_button('Log in')
     end
