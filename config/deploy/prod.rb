400770ceecdc6974142b928e7ee0513afc8f3b86
Alicia Cozine <alicia@curationexperts.com>
2017-08-17 22:46:57 -0500

    need to use prod IP until launch

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,5 +1,5 @@
 # deploys to Emory production machine
 set :stage, :prod
 set :rails_env, 'production'
-server 'etd.library.emory.edu', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
+server '13.59.122.177', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
 append :linked_files, "config/initializers/hyrax.rb", "config/environments/production.rb"
