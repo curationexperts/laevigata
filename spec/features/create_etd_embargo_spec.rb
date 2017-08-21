1aee46dce8c43400de67b2e2f4dba3b77784e1e4
Jennifer Lindner <jenlindner@gmail.com>
2017-08-21 10:51:32 -0500

    school dropdown on embargo tab contains each school as unique option

--- spec/features/create_etd_embargo_spec.rb
+++ spec/features/create_etd_embargo_spec.rb
@@ -22,7 +22,7 @@ RSpec.feature 'Create an Etd: My Embargoes' do
       expect(page).to have_css('#save_my_embargoes')
     end
 
-    scenario "Checking 'No embargoes' makes invalid form valid", js: true do
+    scenario "Checking 'No embargoes' makes invalid form valid", js: true unless continuous_integration? do
       click_on("Embargoes")
 
       expect(page).to have_css('li#required-embargoes.incomplete')
@@ -76,7 +76,33 @@ RSpec.feature 'Create an Etd: My Embargoes' do
       expect(find("#etd_abstract_embargoed", visible: false).value).to eq("true")
     end
 
-    scenario "Selecting embargo types and embargo length makes invalid form valid", js: true unless continuous_integration? do
+    scenario "each school has its own option", js: true unless continuous_integration? do
+      click_on("Embargoes")
+
+      expect(page).to have_select('embargo_school', options: ['', 'Undergraduate Honors', 'Rollins School of Public Health', 'Candler School of Theology', 'Laney Graduate School'])
+    end
+
+    scenario "each school sets the correct embargo lengths", js: true unless continuous_integration? do
+      click_on("Embargoes")
+
+      select('Laney Graduate School', from: 'embargo_school')
+
+      expect(page).to have_select('etd_embargo_length', options: ["", "6 months", "1 year", "2 years", "6 years"])
+
+      select('Undergraduate Honors', from: 'embargo_school')
+
+      expect(page).to have_select('etd_embargo_length', options: ["", "6 months", "1 year", "2 years"])
+
+      select('Rollins School of Public Health', from: 'embargo_school')
+
+      expect(page).to have_select('etd_embargo_length', options: ["", "6 months", "1 year", "2 years"])
+
+      select('Candler School of Theology', from: 'embargo_school')
+
+      expect(page).to have_select('etd_embargo_length', options: ["", "6 months", "1 year", "2 years"])
+    end
+
+    scenario "Selecting embargo types and embargo length makes invalid form valid", js: true do
       click_on("Embargoes")
 
       expect(page).to have_content('What do you want to embargo?')
