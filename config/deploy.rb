59c91b14f4138ecdd400e7df6a7e7d9b2127f979
Bess Sadler <bess@curationexperts.com>
2017-08-21 12:44:05 -0400

    Revert "Tripartite"

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,3 @@
-set :whenever_command, "bundle exec whenever"
-require "whenever/capistrano"
-
 after 'deploy:published', 'laevigata:workflow_setup'
 # config valid only for current version of Capistrano
 lock "3.8.2"
--- config/deploy/qa.rb
+++
@@ -1,5 +0,0 @@
-# deploys to Emory qa machine
-set :stage, :qa
-set :rails_env, 'production'
-server 'qa-etd.library.emory.edu', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
-append :linked_files, "config/initializers/hyrax.rb", "config/environments/production.rb"
