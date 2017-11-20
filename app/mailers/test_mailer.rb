f00d75429b7490dc2fdb7798e4e0b6d59d19febd
Bess Sadler <bess@curationexperts.com>
2017-11-20 10:48:51 -0500

    Comment for Test mailer so we remember why it's there

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,3 +1,7 @@
+# This is a convenience to test whether mail is set up correctly on a given server.
+# To use it, change the email address to your own, launch a rails console and run
+# `TestMailer.test_email.deliver`
+
 class TestMailer < ApplicationMailer
   def test_email
     mail(
