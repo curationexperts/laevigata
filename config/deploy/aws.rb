91717a474c7e07a6e3009b1bb33bcbf2e1dbad47
Bess Sadler <bess@curationexperts.com>
2017-11-29 16:20:08 -0500

    Remove no longer used cap deploy targets

--- config/deploy/aws.rb
+++
@@ -1,5 +0,0 @@
-# deploys to DCE sandbox
-set :stage, :aws
-set :rails_env, 'production'
-server 'emory.curationexperts.com', user: 'deploy', roles: [:web, :app, :db, :resque_pool]
-append :linked_files, "config/initializers/hyrax.rb", "config/environments/production.rb"
