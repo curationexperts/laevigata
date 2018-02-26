ed2986d46210c38532cb0b50b3eedc61328136fc
Bess Sadler <bess@curationexperts.com>
2018-02-26 17:58:07 -0500

    Do not replace production.rb with the version on the server

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,4 +2,3 @@
 set :stage, :staging
 set :rails_env, 'production'
 server 'staging-etd.library.emory.edu', user: 'deploy', roles: [:web, :app, :db]
-append :linked_files, "config/initializers/hyrax.rb", "config/environments/production.rb"
