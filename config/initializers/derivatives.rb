38679ffce9874c78b8c30e70e49689b3dd5bf83a
Tom Johnson <tom@dp.la>
2018-03-22 08:57:19 -0700

    Don't generate derivatives for office document mime types
    Office document mime types lead us to convert (large!) documents to .pdf before
    generating a .jpg. This is resource intensive, and not necessary in this
    application (we are always more concerned about the primary PDF content, and
    could generate the .doc thumbnail from that if need).
    
    We disable derivatives generation by making the relevant method a no-op.

--- config/initializers/derivatives.rb
+++ config/initializers/derivatives.rb
@@ -1,1 +1,4 @@
 Hydra::Derivatives::DocumentDerivatives.processor_class.timeout = 5.minutes
+
+# Don't attempt to create derivatives for `file_set.class.office_document_mime_types`
+Hyrax::FileSetDerivativesService.redefine_method(:create_office_document_derivatives) { |*_args| }
