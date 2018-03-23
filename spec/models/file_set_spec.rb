e5ec731ab9b90d9a09941f059378de4c22740ffb
Tom Johnson <tom@dp.la>
2018-03-23 10:28:01 -0700

    Add 'premis' to supported `pcdm_use` values
    Adds a `FileSet#premis?` method, and fixes logic of `#supplementary?` to allow
    PREMIS files to be a true third option. `#supplementary? => true` now mean
    not-primary && not-premis.

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -1,17 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe FileSet do
-  describe 'primary' do
-    context 'with a new FileSet' do
-      subject { described_class.new }
+  context 'with a new FileSet' do
+    its(:pcdm_use) { is_expected.to be_nil }
+    its(:embargo_length) { is_expected.to be_nil }
+    its(:premis?) { is_expected.to be false }
+    its(:primary?) { is_expected.to be false }
+    its(:supplementary?) { is_expected.to be true }
+    its(:title) { is_expected.to eq [] }
+    its(:description) { is_expected.to eq [] }
+    its(:file_type) { is_expected.to be_nil }
+  end
 
-      its(:pcdm_use) { is_expected.to be_nil }
-      its(:embargo_length) { is_expected.to be_nil }
-      its(:primary?) { is_expected.to be false }
-      its(:supplementary?) { is_expected.to be true }
-      its(:title) { is_expected.to eq [] }
-      its(:description) { is_expected.to eq [] }
-      its(:file_type) { is_expected.to be_nil }
-    end
+  context 'when premis' do
+    subject(:file_set) { described_class.new(pcdm_use: described_class::PREMIS) }
+
+    its(:pcdm_use) { is_expected.to eq described_class::PREMIS }
+
+    it { is_expected.to be_premis }
+    it { is_expected.not_to be_primary }
+    it { is_expected.not_to be_supplementary }
+  end
+
+  context 'when primary' do
+    subject(:file_set) { described_class.new(pcdm_use: described_class::PRIMARY) }
+
+    its(:pcdm_use) { is_expected.to eq described_class::PRIMARY }
+
+    it { is_expected.not_to be_premis }
+    it { is_expected.to be_primary }
+    it { is_expected.not_to be_supplementary }
+  end
+
+  context 'when supplementary' do
+    subject(:file_set) { described_class.new(pcdm_use: described_class::SUPPLEMENTARY) }
+
+    its(:pcdm_use) { is_expected.to eq described_class::SUPPLEMENTARY }
+
+    it { is_expected.not_to be_premis }
+    it { is_expected.not_to be_primary }
+    it { is_expected.to be_supplementary }
   end
 end
