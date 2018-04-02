6f183b4a0dbccf11c9fcfb9b0a9a5bfacb2f5404
Tom Johnson <tom@dp.la>
2018-04-02 16:57:48 -0700

    Use a factory method for department and subfield services
    Hiding this behavior in the `EtdHelper` makes it hard to test. Using a factory
    method allows us to easily check that a given `school` or `department` yields a
    correct service result.
    
    For now, the factory just uses a case statement. We could switch to a registry
    or some other pattern for matching schools or departments to their respective
    services.
    
    Several services had been renamed, but not corrected in bcb83332. These are
    fixed here and regression tests are added.
    
    Connected to #924.

--- app/services/hyrax/laevigata_authority_service.rb
+++ app/services/hyrax/laevigata_authority_service.rb
@@ -7,6 +7,70 @@ module Hyrax
   #
   # @see https://github.com/samvera/questioning_authority/#list-of-id-and-term-keys-and-optionally-active-key
   class LaevigataAuthorityService < Hyrax::QaSelectService
+    class << self
+      ##
+      # Builds an authority service for a given school or department.
+      #
+      # Given a school, a deparmental authority service is returned with the
+      # appropriate deparments within that school. Given a department, a
+      # subfield authority service is returned representing the subfields
+      # within the department.
+      #
+      # If both are given, an argument error is raised.
+      #
+      # @overload for(school:)
+      #   @param [#=~] school
+      #   @return [LaevigataAuthorityService] a departmental authority service
+      # @overload for(departement:)
+      #   @param [#=~] department
+      #   @return [LaevigataAuthorityService] a subfield authority service
+      #
+      # @raise [ArgumentError] when both a school and department are given.
+      def for(school: nil, department: nil)
+        return for_school(school)         if school     && !department
+        return for_department(department) if department && !school
+
+        raise(ArgumentError,
+              "Expected one of school or department. Got school: #{school}; department: #{department}")
+      end
+
+      ##
+      # @api private
+      def for_school(school)
+        case school
+        when /Emory/
+          Hyrax::EmoryService.new
+        when /Laney/
+          Hyrax::LaneyService.new
+        when /Candler/
+          Hyrax::CandlerService.new
+        when /Rollins/
+          Hyrax::RollinsService.new
+        end
+      end
+
+      ##
+      # @api private
+      def for_department(department)
+        case department
+        when 'Business'
+          Hyrax::BusinessService.new
+        when 'Executive Masters of Public Health - MPH'
+          Hyrax::ExecutiveService.new
+        when 'Biostatistics and Bioinformatics'
+          Hyrax::BiostatisticsService.new
+        when 'Environmental Health'
+          Hyrax::EnvironmentalService.new
+        when 'Epidemiology'
+          Hyrax::EpidemiologyService.new
+        when 'Psychology'
+          Hyrax::PsychologyService.new
+        when /Religion/
+          Hyrax::ReligionService.new
+        end
+      end
+    end
+
     def active?(id)
       authority.find(id).fetch('active', true)
     end
