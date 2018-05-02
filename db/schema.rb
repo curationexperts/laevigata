6810a362d02ddff9c7c8a6a54b35bbf5accbfb2b
Tom Johnson <tom@dp.la>
2018-05-02 10:40:02 -0700

    [Hyrax-2.0] Run `rails db:migrate`
    Run the Hyrax 2 migrations to update `schema.rb`.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170824175459) do
+ActiveRecord::Schema.define(version: 20180502172935) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -27,12 +27,13 @@ ActiveRecord::Schema.define(version: 20170824175459) do
   create_table "checksum_audit_logs", force: :cascade do |t|
     t.string   "file_set_id"
     t.string   "file_id"
-    t.string   "version"
-    t.integer  "pass"
+    t.string   "checked_uri"
     t.string   "expected_result"
     t.string   "actual_result"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "passed"
+    t.index ["checked_uri"], name: "index_checksum_audit_logs_on_checked_uri"
     t.index ["file_set_id", "file_id"], name: "by_file_set_id_and_file_id"
   end
 
@@ -101,6 +102,20 @@ ActiveRecord::Schema.define(version: 20170824175459) do
     t.boolean  "enabled",    default: false, null: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "job_io_wrappers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "uploaded_file_id"
+    t.string   "file_set_id"
+    t.string   "mime_type"
+    t.string   "original_name"
+    t.string   "path"
+    t.string   "relation"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["uploaded_file_id"], name: "index_job_io_wrappers_on_uploaded_file_id"
+    t.index ["user_id"], name: "index_job_io_wrappers_on_user_id"
   end
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
@@ -174,6 +189,7 @@ ActiveRecord::Schema.define(version: 20170824175459) do
     t.string   "access"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["permission_template_id", "agent_id", "agent_type", "access"], name: "uk_permission_template_accesses", unique: true
   end
 
   create_table "permission_templates", force: :cascade do |t|
@@ -491,6 +507,7 @@ ActiveRecord::Schema.define(version: 20170824175459) do
     t.string   "provider"
     t.string   "ppid"
     t.string   "uid"
+    t.string   "preferred_locale"
     t.index ["ppid"], name: "index_users_on_ppid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
