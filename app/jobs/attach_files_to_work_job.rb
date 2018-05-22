063995b2cea3d913e723c89f51d5403e84fa93b5
Tom Johnson <tom@dp.la>
2018-05-22 11:26:15 -0700

    [Hyrax-2.0] Reinstate virus checking
    The virus check here is slightly refactored from the original one; we more
    explictly check and raise a specific error, instead of waiting for save to
    fail. We also use `ActiveRecord::Base#find_by`, which is more explict and
    efficient than doing `.where(key: value).first`.

--- app/jobs/attach_files_to_work_job.rb
+++ app/jobs/attach_files_to_work_job.rb
@@ -11,6 +11,7 @@ class AttachFilesToWorkJob < Hyrax::ApplicationJob
     work_permissions = work.permissions.map(&:to_hash)
     metadata = visibility_attributes(work_attributes)
     uploaded_files.each do |uploaded_file|
+      virus_check!(uploaded_file)
       actor = Hyrax::Actors::FileSetActor.new(FileSet.create, user)
       actor.create_metadata(metadata)
       actor.create_content(uploaded_file)
@@ -26,9 +27,29 @@ class AttachFilesToWorkJob < Hyrax::ApplicationJob
       actor.file_set.permissions_attributes = work_permissions
       uploaded_file.update(file_set_uri: actor.file_set.uri)
     end
+  rescue VirusDetectedError
+    Rails.logger.error "Virus encountered while processing work #{work.id}."
+
+    work_global_id = work.to_global_id.to_s
+    entity         = Sipity::Entity.find_by(proxy_for_global_id: work_global_id)
+
+    Hyrax::Workflow::VirusEncounteredNotification
+      .send_notification(entity:     entity,
+                         comment:    '',
+                         user:       ::User.find_by(ppid: WorkflowSetup::NOTIFICATION_OWNER),
+                         recipients: {})
   end
 
+  class VirusDetectedError < RuntimeError; end
+
   private
+
+    def virus_check!(uploaded_file)
+      return unless Hydra::Works::VirusCheckerService.file_has_virus?(uploaded_file.file)
+      carrierwave_file = uploaded_file.file.file
+      carrierwave_file.delete
+      raise(VirusDetectedError, carrierwave_file.filename)
+    end
 
     # The attributes used for visibility - sent as initial params to created FileSets.
     def visibility_attributes(attributes)
@@ -52,54 +73,3 @@ class AttachFilesToWorkJob < Hyrax::ApplicationJob
       work.on_behalf_of.blank? ? work.depositor : work.on_behalf_of
     end
 end
-
-# Converts UploadedFiles into FileSets and attaches them to works.
-# class AttachFilesToWorkJob < ActiveJob::Base
-#   queue_as Hyrax.config.ingest_queue_name
-
-#   # @param [ActiveFedora::Base] work - the work object
-#   # @param [Array<UploadedFile>] uploaded_files - an array of files to attach
-#   def perform(work, uploaded_files)
-#     uploaded_files.each do |uploaded_file|
-#       file_set = FileSet.new
-#       user = User.find_by_user_key(work.depositor)
-#       actor = Hyrax::Actors::FileSetActor.new(file_set, user)
-#       actor.create_metadata(visibility: work.visibility)
-#       attach_content(actor, uploaded_file.file)
-#       actor.attach_file_to_work(work)
-#       actor.file_set.permissions_attributes = work.permissions.map(&:to_hash)
-#       file_set.pcdm_use = uploaded_file.pcdm_use
-#       if uploaded_file.pcdm_use == 'primary'
-#         file_set.title = work.title
-#       else
-#         file_set.title = Array.wrap(uploaded_file.title)
-#         file_set.description = Array.wrap(uploaded_file.description)
-#         file_set.file_type = uploaded_file.file_type
-#       end
-#       file_set.save
-#       uploaded_file.update(file_set_uri: file_set.uri)
-#     end.
-#   rescue ActiveFedora::RecordInvalid => e
-#     # If the file set cannot be saved because a virus was encountered,
-#     # delete the file and notify the depositor and approvers
-#     raise e unless e.to_s =~ /virus/
-#     Rails.logger.error "Virus encountered while processing work #{work.id}."
-#     work_global_id = work.to_global_id.to_s
-#     entity = Sipity::Entity.where(proxy_for_global_id: work_global_id).first
-#     Hyrax::Workflow::VirusEncounteredNotification.send_notification(entity: entity, comment: '', user: ::User.where(ppid: WorkflowSetup::NOTIFICATION_OWNER).first, recipients: nil)
-#   end
-
-#   private
-
-#     # @param [Hyrax::Actors::FileSetActor] actor
-#     # @param [Hyrax::UploadedFileUploader] file file.file must be a CarrierWave::SanitizedFile or file.url must be present
-#     def attach_content(actor, file)
-#       if file.file.is_a? CarrierWave::SanitizedFile
-#         actor.create_content(file.file.to_file)
-#       elsif file.url.present?
-#         actor.import_url(file.url)
-#       else
-#         raise ArgumentError, "#{file.class} received with #{file.file.class} object and no URL"
-#       end
-#     end
-# end
