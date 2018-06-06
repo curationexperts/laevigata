dc87df1eb7ec22b5afd47497204fe52be6d568a9
Jennifer Lindner <jenlindner@gmail.com>
2018-06-06 12:45:59 -0400

    adding simple model for in-progress-etds

---
+++ db/migrate/20180606164027_create_in_progress_etds.rb
@@ -0,0 +1,12 @@
+class CreateInProgressEtds < ActiveRecord::Migration[5.0]
+  def change
+    create_table :in_progress_etds do |t|
+      t.string :name
+      t.string :email
+      t.string :graduation_date
+      t.string :submission_type
+
+      t.timestamps
+    end
+  end
+end
