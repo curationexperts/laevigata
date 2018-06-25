5ba5182b3e4bb13510355ec1cea172bd215d6f52
Bess Sadler <bess@curationexperts.com>
2018-06-25 14:18:04 -0400

    Fix deploy target

--- config/deploy/prod_upgrade.rb
+++ config/deploy/prod_upgrade.rb
@@ -1,4 +1,4 @@
 # deploys to Emory production upgrade machine
 set :stage, :prod_upgrade
 set :rails_env, 'production'
-server 'prod-upgrade-etd.emory.edu', user: 'deploy', roles: [:web, :app, :db]
+server 'upgrade-etd.emory.edu', user: 'deploy', roles: [:web, :app, :db]
