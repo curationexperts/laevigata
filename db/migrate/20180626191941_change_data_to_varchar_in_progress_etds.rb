812722898f24177d62441c2199070f7a67424012
Mark Bussey <mark@curationexperts.com>
2018-06-27 10:13:27 -0500

    Revert "change datatype of json field from nvarchar to varchar"
    This reverts commit 922dcd3f4841acf71bc3037553c17471b7a304de.

--- db/migrate/20180626191941_change_data_to_varchar_in_progress_etds.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDataToVarcharInProgressEtds < ActiveRecord::Migration[5.1]
-  def change
-    #change_column :in_progress_etds, :data, :varchar
-    reversible do |column|
-        column.up   { change_column :in_progress_etds, :data, :varchar }
-        column.down { change_column :in_progress_etds, :data, :nvarchar }
-    end
-  end
-end
