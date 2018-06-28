12e6c1d088128578f76f3ea7f02347d04185859b
Jennifer Lindner <jenlindner@gmail.com>
2018-06-28 11:55:31 -0400

    authenticated vue posts

--- app/javascript/packs/root.js
+++ app/javascript/packs/root.js
@@ -2,14 +2,18 @@ import TurbolinksAdapter from 'vue-turbolinks'
 import Vue from 'vue/dist/vue.esm'
 import App from '../app.vue'
 import { formStore } from '../form_store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-Vue.use(TurbolinksAdapter)
+Vue.use(TurbolinksAdapter, axios, VueAxios)
 
 document.addEventListener('turbolinks:load', () => {
+  let token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  axios.defaults.headers.common['X-CSRF-Token'] = token
+
   const app = new Vue({
     el: '#root',
     data: formStore,
     components: { App }
   })
 })
-
