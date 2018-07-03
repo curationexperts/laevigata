f3c2d014951cd39d6df2166f4d3b0e9548b23d93
Bess Sadler <bess@curationexperts.com>
2018-07-03 13:49:00 -0400

    Restore the Manage Users option to admin dashboard
    Fix the user profile path link
    Add the Manage Users link to the admin dashboard
    Add a feature test

---
+++ spec/features/admin_dashboard_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.feature 'Admin dashboard',
+              workflow: { admin_sets_config: 'spec/fixtures/config/emory/epidemiology_admin_sets.yml' } do
+
+  context 'as an admin user' do
+    let(:user) { FactoryBot.create(:admin) }
+
+    scenario 'view the admin statistics page' do
+      login_as user
+      visit '/dashboard'
+      click_on 'Reports'
+      expect(page).to have_content 'Work Statistics'
+    end
+
+    scenario 'manage users and see admin users' do
+      login_as user
+      visit '/dashboard'
+      click_on 'Manage Users'
+      expect(page).to have_content 'Last access'
+    end
+
+    scenario 'school config' do
+      login_as user
+      visit '/dashboard'
+      click_on 'Schools'
+      expect(page).to have_content 'Candler School of Theology'
+    end
+  end
+end
