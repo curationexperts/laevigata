f554f8d09d378c411aa4e93dc3a8818024f0c59e
Jennifer Lindner <jenlindner@gmail.com>
2018-07-05 09:28:19 -0400

    adding other validators

--- app/models/in_progress_etd.rb
+++ app/models/in_progress_etd.rb
@@ -2,4 +2,8 @@ class InProgressEtd < ApplicationRecord
   # custom validators check for presence of tab-determined set of fields based on presence of tab-identifying data
   validates_with AboutMeValidator
   validates_with MyProgramValidator
+  validates_with MyEtdValidator
+  validates_with MyAdvisorValidator
+  validates_with KeywordValidator
+  validates_with EmbargoValidator
 end
--- spec/models/in_progress_etd_spec.rb
+++ spec/models/in_progress_etd_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe InProgressEtd do
+  let(:in_progress_etd) { described_class.new(data: data.to_json) }
   before(:all) do
     new_ui = Rails.application.config_for(:new_ui).fetch('enabled', false)
 
@@ -12,7 +13,6 @@ describe InProgressEtd do
       let(:data) do
         { currentTab: "About Me", creator: "Student", graduation_date: "tomorrow", post_graduation_email: "tester@test.com", school: "Laney Graduate School" }
       end
-      let(:in_progress_etd) { described_class.new(data: data.to_json) }
 
       it "is valid" do
         expect(in_progress_etd).to be_valid
@@ -26,6 +26,13 @@ describe InProgressEtd do
         expect(in_progress_etd).not_to be_valid
       end
     end
+    context "with missing data" do
+      let(:in_progress_etd) { described_class.new(data: { currentTab: "About Me", creator: "Student", post_graduation_email: "" }.to_json) }
+
+      it "is not valid" do
+        expect(in_progress_etd).not_to be_valid
+      end
+    end
   end
 
   describe "My Program" do
@@ -33,7 +40,6 @@ describe InProgressEtd do
       let(:data) do
         { currentTab: "My Program", department: "Anthropology", subfield: "Foo", partnering_agency: "DCD", degree: "Non", submitting_type: "Phd" }
       end
-      let(:in_progress_etd) { described_class.new(data: data.to_json) }
 
       it "is valid" do
         expect(in_progress_etd).to be_valid
@@ -44,11 +50,89 @@ describe InProgressEtd do
       let(:data) do
         { currentTab: "My Program", department: "", subfield: "Foo", partnering_agency: "DCD", degree: "Non", submitting_type: "" }
       end
-      let(:in_progress_etd) { described_class.new(data: data.to_json) }
 
       it "is not valid" do
         expect(in_progress_etd).not_to be_valid
       end
+    end
+  end
+
+  describe "My Advisor" do
+    # TODO: align with committee front end if it changes, which it is likely to
+    context "with valid data" do
+      let(:data) do
+        { currentTab: "My Advisor", "committee_member_attributes": ["member_info"], "committee_chair_attributes": ["chair_info"] }
+      end
+
+      it "is valid" do
+        expect(in_progress_etd).to be_valid
+      end
+    end
+
+    context "without valid data" do
+      let(:data) do
+        { currentTab: "My Advisor" }
+      end
+
+      it "is not valid" do
+        expect(in_progress_etd).not_to be_valid
+      end
+    end
+  end
+
+  describe "My ETD" do
+    context "with valid data" do
+      let(:data) do
+        { currentTab: "My Etd", "title": "A title", "language": "Francais", "abstract": "<b>Abstract</b>", "table_of_contents": "<i>Chapter One</i>" }
+      end
+
+      it "is valid" do
+        expect(in_progress_etd).to be_valid
+      end
+    end
+
+    context "with invalid data" do
+      let(:data) do
+        { currentTab: "My Etd", "title": "", "language": "", "abstract": "", "table_of_contents": "" }
+      end
+
+      it "is not valid" do
+        expect(in_progress_etd).not_to be_valid
+      end
+    end
+  end
+
+  # TODO: needs to be adjusted to fit real data structure when tab is built according to wireframe
+  describe "Keywords" do
+    context "with valid data" do
+      let(:data) do
+        { currentTab: "Keywords", "keyword": "something", "research_field": ["things"] }
+      end
+
+      it "is valid" do
+        expect(in_progress_etd).to be_valid
+      end
+    end
+
+    context "with invalid data" do
+      let(:data) do
+        { currentTab: "Keywords" }
+      end
+
+      it "is not valid" do
+        expect(in_progress_etd).not_to be_valid
+      end
+    end
+  end
+
+  # TODO: the validator needs to be adjusted for real embargo front end tab
+  describe "Embargo" do
+    let(:data) do
+      { currentTab: "Embargo" }
+    end
+
+    it "is valid" do
+      expect(in_progress_etd).to be_valid
     end
   end
 end
