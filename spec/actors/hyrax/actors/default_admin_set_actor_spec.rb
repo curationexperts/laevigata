89050ac83d2cc8e5c6b1e1929a422708dbddcc94
Bess Sadler <bess@curationexperts.com>
2018-07-05 12:25:28 -0400

    Refactor for clarity and to remove deprecated actor stack calls

--- spec/actors/hyrax/actors/default_admin_set_actor_spec.rb
+++ spec/actors/hyrax/actors/default_admin_set_actor_spec.rb
@@ -2,45 +2,41 @@ libdir = File.expand_path('../../../../', __FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 require 'rails_helper'
 
-RSpec.describe Hyrax::Actors::DefaultAdminSetActor do
-  before :all do
-    ActiveFedora::Cleaner.clean!
-    DatabaseCleaner.strategy = :truncation
-    DatabaseCleaner.clean
-  end
-  let(:next_actor) { double('next actor', create: true) }
-  let(:actor) do
-    Hyrax::Actors::ActorStack.new(etd, depositor_ability, [described_class])
+RSpec.describe Hyrax::Actors::DefaultAdminSetActor, :clean do
+  subject(:middleware) do
+    stack = ActionDispatch::MiddlewareStack.new.tap do |middleware|
+      middleware.use described_class
+    end
+    stack.build(terminator)
   end
   let(:depositor) { create(:user) }
-  let(:depositor_ability) { ::Ability.new(depositor) }
+  let(:ability) { ::Ability.new(depositor) }
   let(:etd) { FactoryBot.create(:sample_data, admin_set: nil, school: ["Laney Graduate School"]) }
+  let(:terminator) { Hyrax::Actors::Terminator.new }
   let(:admin_set) { FactoryBot.create(:admin_set, title: ["Laney Graduate School"], id: "f1881k888") }
 
   describe "create" do
-    before do
-      allow(Hyrax::Actors::Terminator).to receive(:new).and_return(next_actor)
-      allow(next_actor).to receive(:create).with(admin_set_id: admin_set.id).and_return(true)
-    end
-
     context "when there is no admin_set assigned" do
-      let(:attributes) { { admin_set_id: '' } }
-      it "assigns the admin_set and admin_set id" do
-        actor.create(attributes)
-        expect(next_actor)
-          .to have_received(:create)
-          .with(have_attributes(attributes: { admin_set_id: admin_set.id }))
+      let(:attributes) { {} }
+      let(:env) { Hyrax::Actors::Environment.new(etd, ability, attributes) }
+
+      it "assigns the admin_set id" do
+        expect(etd.admin_set_id).to be_nil
+        admin_set
+        middleware.create(env)
+        expect(etd.admin_set_id).to eq admin_set.id
       end
     end
 
     context "when admin_set_id is provided" do
       let(:attributes) { { admin_set_id: admin_set.id } }
+      let(:env) { Hyrax::Actors::Environment.new(etd, ability, attributes) }
 
       it "keeps the admin_set id" do
-        actor.create(attributes)
-        expect(next_actor)
-          .to have_received(:create)
-          .with(have_attributes(attributes: attributes))
+        expect(etd.admin_set_id).to be_nil
+        admin_set
+        middleware.create(env)
+        expect(etd.admin_set_id).to eq admin_set.id
       end
     end
   end
