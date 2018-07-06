3a0fa63b78785e8c184d3127a32410d4487364e6
Bess Sadler <bess@curationexperts.com>
2018-07-06 13:50:43 -0400

    Refactor to use new ActorStack methods
    Also, refactor to do the test setup once and then run
    all examples together. This cuts the execution time from ~27 seconds
    to ~7 seconds.

--- spec/services/hyrax/workflow/unhidden_notification_spec.rb
+++ spec/services/hyrax/workflow/unhidden_notification_spec.rb
@@ -3,39 +3,35 @@ $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 require 'rails_helper'
 require 'workflow_setup'
 
-RSpec.describe Hyrax::Workflow::UnhiddenNotification, :clean do
-  before do
-    w = WorkflowSetup.new("#{fixture_path}/config/emory/superusers.yml", "#{fixture_path}/config/emory/candler_admin_sets.yml", "/dev/null")
-    w.setup
-  end
+RSpec.describe Hyrax::Workflow::UnhiddenNotification, :clean, workflow: { admin_sets_config: 'spec/fixtures/config/emory/candler_admin_sets.yml' } do
   let(:user) { FactoryBot.create(:user) }
   let(:etd) { FactoryBot.create(:sample_data, depositor: user.user_key, school: ["Candler School of Theology"]) }
   let(:ability) { ::Ability.new(user) }
   let(:recipients) do
     { 'to' => [FactoryBot.create(:user), FactoryBot.create(:user)] }
   end
+  let(:attributes) { {} }
+  let(:env) { Hyrax::Actors::Environment.new(etd, ability, attributes) }
+  let(:terminator) { Hyrax::Actors::Terminator.new }
+  let(:middleware) do
+    Hyrax::DefaultMiddlewareStack.build_stack.build(terminator)
+  end
   let(:notification) do
-    env = Hyrax::Actors::Environment.new(etd, ability, {})
-    Hyrax::CurationConcern.actor.create(env)
+    middleware.create(env)
     work_global_id = etd.to_global_id.to_s
     entity = Sipity::Entity.where(proxy_for_global_id: work_global_id).first
     described_class.new(entity, '', user, recipients)
   end
-  it "can instantiate" do
-    expect(notification).to be_instance_of(described_class)
-  end
-  it "has the full url in the message" do
+  it "acts like a notification" do
+    # it "has the full url in the message"
     expect(notification.message).to match(/http/)
-  end
-  it "can find depositor" do
+    # it "can find depositor"
     expect(notification.depositor).to be_instance_of(::User)
     expect(notification.depositor.uid).to eq user.uid
-  end
-  it "can find approvers" do
+    # it "can find approvers"
     expect(notification.approvers).to be_instance_of(Array)
     expect(notification.approvers.pluck(:uid)).to contain_exactly("admin_set_owner", "superman001", "wonderwoman001", "tezprox", "candleradmin", "candleradmin2")
-  end
-  it "sends notifications to the depositor, school approvers and superusers and no one else" do
+    # it "sends notifications to the depositor, school approvers and superusers and no one else"
     expect(notification.recipients["to"].pluck(:uid)).to contain_exactly("admin_set_owner", "superman001", "wonderwoman001", "tezprox", "candleradmin", "candleradmin2", user.uid)
   end
 end
