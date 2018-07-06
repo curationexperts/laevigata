16280c8ea8c88062b6b1925dfabb27c2cd3f3241
Bess Sadler <bess@curationexperts.com>
2018-07-06 12:11:20 -0400

    Actually attach files with UploadedFile factory
    There are many places in our test suite where we
    could use this factory if it actually attached files.
    Fixing this should let us refactor and streamline
    many of our tests.

--- spec/factories/uploaded_file.rb
+++ spec/factories/uploaded_file.rb
@@ -1,11 +1,30 @@
 FactoryBot.define do
   factory :uploaded_file, class: Hyrax::UploadedFile do
-    file { "fake_title.pdf" }
+    file { Rack::Test::UploadedFile.new("#{::Rails.root}/spec/fixtures/joey/joey_thesis.pdf") }
     user_id { 1 }
-    pcdm_use { "supplementary" }
+    pcdm_use { FileSet::SUPPLEMENTARY }
 
     factory :primary_uploaded_file, traits: [:primary]
-    factory :supplementary_uploaded_file, traits: [:supplementary]
+
+    factory :supplementary_uploaded_file, traits: [:supplementary] do
+      file { Rack::Test::UploadedFile.new("#{::Rails.root}/spec/fixtures/miranda/image.tif") }
+      description { 'Description of the supplementary file' }
+      file_type { 'Image' }
+    end
+
+    factory :uploaded_image_file, traits: [:supplementary] do
+      file { Rack::Test::UploadedFile.new("#{::Rails.root}/spec/fixtures/miranda/image.tif") }
+      title { FFaker::Animal.common_name }
+      description { "portrait" }
+      file_type { 'Image' }
+    end
+
+    factory :uploaded_data_file, traits: [:supplementary] do
+      file { Rack::Test::UploadedFile.new("#{::Rails.root}/spec/fixtures/miranda/rural_clinics.zip") }
+      title { "Rural Clinics in Georgia" }
+      description { "GIS shapefile showing rural clinics" }
+      file_type { 'Dataset' }
+    end
 
     trait :primary do
       pcdm_use { FileSet::PRIMARY }
