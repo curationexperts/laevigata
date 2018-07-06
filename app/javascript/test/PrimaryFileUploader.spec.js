68899ea964f7e53eb6f687e9e9943993226b4b24
Jamie Little <jamie@jamielittle.org>
2018-07-06 14:04:26 -0500

    Add file upload progress to the form and limit to PDF
    These are basic enhancements to the form, including a progress
    bar for the file uploader and the HTML5 file upload file type
    validation attribute.

--- app/javascript/test/PrimaryFileUploader.spec.js
+++ app/javascript/test/PrimaryFileUploader.spec.js
@@ -8,6 +8,6 @@ describe('PrimaryFileUploader.vue', () => {
   it('has the correct html', () => {
     const wrapper = shallowMount(PrimaryFileUploader, {
     })
-    expect(wrapper.html()).toContain('<div><input name="primary_files[]" type="file"> </div>')
+    expect(wrapper.html()).toContain('<div><input name="primary_files[]" type="file" accept="application/pdf"> <br> <div class="progress"><div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="progress-bar" style="width: 0%;"></div></div> </div>')
   })
 })
