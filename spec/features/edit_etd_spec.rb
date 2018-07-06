358e8bcf888b9747f1d76a3cce9e5c639965abe7
Bess Sadler <bess@curationexperts.com>
2018-07-06 16:07:31 -0400

    Refactor to use updated Actor Stack syntax

--- spec/features/edit_etd_spec.rb
+++ spec/features/edit_etd_spec.rb
@@ -73,7 +73,8 @@ RSpec.feature 'Edit an existing ETD', :perform_jobs, :clean do
 
     attributes_for_actor = { uploaded_files: file_ids }
     env = Hyrax::Actors::Environment.new(etd, ::Ability.new(student), attributes_for_actor)
-    Hyrax::CurationConcern.actor.create(env)
+    middleware = Hyrax::DefaultMiddlewareStack.build_stack.build(Hyrax::Actors::Terminator.new)
+    middleware.create(env)
 
     # Approver requests changes, so student will be able to edit the ETD
     change_workflow_status(etd, "request_changes", approver)
