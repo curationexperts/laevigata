be39a636e1fb703229610be357679dcf388f7819
Bess Sadler <bess@curationexperts.com>
2018-07-10 14:30:50 -0400

    Refactor to use updated Hyrax::DefaultMiddlewareStack (#1273)
    This eliminates deprecation warnings.
    
    Also, refactor workflow notifications to do the test setup once and then run
    all examples together. This cuts the execution time from ~27 seconds
    to ~7 seconds.
    
    Run workflow notifications with integration tests

--- spec/services/hyrax/workflow/changes_required_notification_spec.rb
+++ spec/services/hyrax/workflow/changes_required_notification_spec.rb
@@ -1,46 +1,7 @@
 libdir = File.expand_path('../../../../', __FILE__)
 $LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
 require 'rails_helper'
-require 'workflow_setup'
 
-RSpec.describe Hyrax::Workflow::ChangesRequiredNotification, :clean do
-  before do
-    w = WorkflowSetup.new("#{fixture_path}/config/emory/superusers.yml", "#{fixture_path}/config/emory/candler_admin_sets.yml", "/dev/null")
-    w.setup
-  end
-
-  let(:user) { FactoryBot.create(:user) }
-  let(:ability) { ::Ability.new(user) }
-
-  let(:recipients) do
-    { 'to' => [FactoryBot.create(:user), FactoryBot.create(:user)] }
-  end
-
-  let(:etd) do
-    FactoryBot.actor_create(:sample_data, depositor: user.user_key, school: ["Candler School of Theology"], user: user)
-  end
-
-  let(:notification) do
-    work_global_id = etd.to_global_id.to_s
-    entity = Sipity::Entity.where(proxy_for_global_id: work_global_id).first
-    described_class.new(entity, '', user, recipients)
-  end
-
-  it "can instantiate" do
-    expect(notification).to be_instance_of(described_class)
-  end
-  it "has the full url in the message" do
-    expect(notification.message).to match(/http/)
-  end
-  it "can find depositor" do
-    expect(notification.depositor).to be_instance_of(::User)
-    expect(notification.depositor.uid).to eq user.uid
-  end
-  it "can find approvers" do
-    expect(notification.approvers).to be_instance_of(Array)
-    expect(notification.approvers.pluck(:uid)).to contain_exactly("admin_set_owner", "superman001", "wonderwoman001", "candleradmin", "candleradmin2", "tezprox")
-  end
-  it "sends notifications to the depositor, school approvers and superusers and no one else" do
-    expect(notification.recipients["to"].pluck(:uid)).to contain_exactly("admin_set_owner", "superman001", "wonderwoman001", "candleradmin", "candleradmin2", "tezprox", user.uid)
-  end
+RSpec.describe Hyrax::Workflow::ChangesRequiredNotification do
+  it_behaves_like 'a workflow notification'
 end
