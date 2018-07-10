868fcc0ca523b164ee768af50f3af0c363b152a7
Jamie Little <jamie@jamielittle.org>
2018-07-09 20:47:11 -0500

    Add copyright questions to the form
    This adds the new copyright questions attributes
    to the form. Previously they were `question_one`,etc, but recently they changed to have more meaningful names.

---
+++ app/javascript/CopyrightQuestions.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-for="question in sharedState.copyrightQuestions" v-bind:key="question.name">
+            <label :for="question.name">{{ question.label }}</label>
+            <p>{{ question.text }}</p>
+          <input :id="question.name" class="checkbox copyright-checkbox" :name="question.name" type="checkbox" v-model="question.choice"
+          true-value="yes" false-value="no">
+        </div>
+    </div>
+</template>
+
+<script>
+import Vue from "vue"
+import App from "./App"
+import { formStore } from './formStore'
+export default {
+ data() {
+     return {
+         sharedState: formStore
+     }
+ }
+}
+</script>
+
+<style scoped>
+.copyright-checkbox {
+    margin-bottom: 3em;
+}
+</style>
