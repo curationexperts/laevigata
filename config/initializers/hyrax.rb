b11c51ed1bfe462d5526afb067e84a5823c11c2d
Tom Johnson <tom@curationexperts.com>
2018-07-16 10:17:54 -0700

    Use a local actor instead of overriding `InterpretVisibilityActor`
    Creates a local `LaevigataEmbargo` actor to replace the monkeypatch on
    `InterpretVisibilityActor`. This is just a rename & replace of the existing
    actor, but opens us up to dropping the override entirely.
    
    Connected to #430.

--- config/initializers/hyrax.rb
+++ config/initializers/hyrax.rb
@@ -273,3 +273,4 @@ module Hyrax
 end
 
 Hyrax::CurationConcern.actor_factory.insert_after(Hyrax::Actors::TransactionalRequest, PrimaryFileTitleActor)
+Hyrax::CurationConcern.actor_factory.swap(Hyrax::Actors::InterpretVisibilityActor, Hyrax::Actors::LaevigataEmbargo)
