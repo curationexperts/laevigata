5613686e2af6fa36877f229e5c9fc846f2a468bb
Bess Sadler <bess@curationexperts.com>
2018-07-17 18:03:49 -0400

    Restart apache after a capistrano deploy (#1331)
    Capistrano passenger restart isn't working consistently,
    so restart apache2 after a successful deploy, to ensure
    changes are picked up.

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,3 +51,14 @@ namespace :sidekiq do
     end
   end
 end
+
+# Capistrano passenger restart isn't working consistently,
+# so restart apache2 after a successful deploy, to ensure
+# changes are picked up.
+namespace :deploy do
+  after :finishing, :restart_apache do
+    on roles(:app) do
+      execute :sudo, :systemctl, :restart, :apache2
+    end
+  end
+end
