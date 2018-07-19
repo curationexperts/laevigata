2a1af0a520b6ddb74a5bc63a70d650bab1204e78
Bess Sadler <bess@curationexperts.com>
2018-07-19 09:51:11 -0400

    Try to fix intermittent box upload error
    Ensure we're iterating over a hash

--- app/controllers/hyrax/etds_controller.rb
+++ app/controllers/hyrax/etds_controller.rb
@@ -150,15 +150,15 @@ module Hyrax
     def merge_selected_files_hashes(params)
       selected_files = {}
       be_files = params.fetch('selected_files', false)
-
+      return unless be_files
       count_of_files = 0
       index = 0
 
       # Get count of all browse-everything selected_files
-      be_files[0].each_key { |k| count_of_files += be_files[0][k].size }
+      be_files[0]&.to_h&.each_key { |k| count_of_files += be_files[0][k].size }
 
       # Populate the selected_files hash with all of the browse-everything files, with keys in the structure the rest of the application will expect: their indexes converted to strings
-      be_files[0].each_key do |k|
+      be_files[0]&.to_h&.each_key do |k|
         be_files[0][k].each do |ke, va| # rubocop:disable HashEachMethods
           if index < count_of_files
             selected_files[index.to_s] = va
