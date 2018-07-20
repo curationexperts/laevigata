32908650642e5b0ed34e68417501e8554f381774
Tom Johnson <tom@curationexperts.com>
2018-07-20 13:14:28 -0700

    Add :committee_chair_attributes to known nested attributes workaround
    Ensures Committee Chairs can save correctly, as described in the fix for #1379.

--- app/actors/hyrax/actors/etd_actor.rb
+++ app/actors/hyrax/actors/etd_actor.rb
@@ -1,6 +1,9 @@
 module Hyrax
   module Actors
     class EtdActor < Hyrax::Actors::BaseActor
+      KNOWN_NESTED_ATTRIBUTES = [:committee_members_attributes,
+                                 :committee_chair_attributes].freeze
+
       private
 
         def apply_save_data_to_curation_concern(env)
@@ -9,7 +12,7 @@ module Hyrax
           # ActiveFedora fails to propgate changes to nested attributes to
           # `#resource` when indexed nested attributes are used. We force the
           # issue here to work around for form edits.
-          (env.attributes.keys && [:committee_members_attributes]).each do |attribute_key|
+          (env.attributes.keys && KNOWN_NESTED_ATTRIBUTES).each do |attribute_key|
             attribute = attribute_key.to_s.gsub('_attributes', '').to_sym
             env.curation_concern.send(attribute).each { |member| member.try(:persist!) }
           end
