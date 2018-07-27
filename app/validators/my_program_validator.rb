a569cca344c9f195db1f47c93cb26d3d8d11cc2b
Jennifer Lindner <jenlindner@gmail.com>
2018-07-27 16:46:35 -0400

    excluding subfields from validation

--- app/validators/my_program_validator.rb
+++ app/validators/my_program_validator.rb
@@ -2,6 +2,8 @@ class MyProgramValidator < ActiveModel::Validator
   def validate(record)
     return unless current_tab?(record)
     ::Hyrax::EtdForm.my_program_terms.each do |field|
+      # TODO: confirm whether subfields are never required by Emory
+      next if field == :subfield
       record.errors.add(field, "#{field} is required") if parsed_data(record)[field.to_s].blank?
     end
   end
