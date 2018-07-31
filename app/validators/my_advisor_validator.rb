6ed7c5de40eda212595c80e260763a644e3500b1
Jennifer Lindner <jenlindner@gmail.com>
2018-07-31 14:42:24 -0400

    committee members are not required for valid submission

--- app/validators/my_advisor_validator.rb
+++ app/validators/my_advisor_validator.rb
@@ -2,6 +2,7 @@ class MyAdvisorValidator < ActiveModel::Validator
   def validate(record)
     return unless current_tab?(record)
     ::Hyrax::EtdForm.my_advisor_terms.each do |field|
+      next if field == :committee_members_attributes
       record.errors.add(field, "#{field} is required") if parsed_data(record)[field.to_s].blank?
     end
   end
