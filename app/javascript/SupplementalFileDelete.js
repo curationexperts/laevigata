cec1569e7fc24cae3ce8e6f917dcc64978ce5da1
Jennifer Lindner <jenlindner@gmail.com>
2018-08-08 16:47:57 -0400

    remove the metadata as well the file

--- app/javascript/SupplementalFileDelete.js
+++ app/javascript/SupplementalFileDelete.js
@@ -10,8 +10,12 @@ export default class SupplementalFileDelete extends FileDelete {
     const filteredFiles = this.formStore.supplementalFiles.filter(
       file => file.deleteUrl !== this.deleteUrl
     )
+    const filteredMetadata = this.formStore.supplementalFilesMetadata.filter(
+      file => file.id !== this.id
+    )
     console.log(filteredFiles)
     this.formStore.supplementalFiles = filteredFiles
+    this.formStore.supplementalFilesMetadata = filteredMetadata
     this.formStore.removeSavedSupplementalFile(this.deleteUrl)
     this.formStore.removeSavedSupplementalFileMetadata(this.id)
   }
