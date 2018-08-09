48f7c96e1aee68adb6fc3da2a2d6d670bbc68b2e
Bess Sadler <bess@curationexperts.com>
2018-08-08 22:17:32 -0400

    Translate embargo_type into expected params
    We have a mis-match between what the new UI
    form is sending for embargo_type and what
    the object is expecting to receive. This
    translates between the two.

--- app/actors/hyrax/actors/etd_actor.rb
+++ app/actors/hyrax/actors/etd_actor.rb
@@ -7,8 +7,8 @@ module Hyrax
       private
 
         def apply_save_data_to_curation_concern(env)
+          translate_embargo_types(env)
           super
-
           # ActiveFedora fails to propgate changes to nested attributes to
           # `#resource` when indexed nested attributes are used. We force the
           # issue here to work around for form edits.
@@ -16,6 +16,17 @@ module Hyrax
             attribute = attribute_key.to_s.gsub('_attributes', '').to_sym
             env.curation_concern.send(attribute).each { |member| member.try(:persist!) }
           end
+        end
+
+        # We must translate a value like env.attributes[:embargo_type] = "files_embargoed, toc_embargoed, abstract_embargoed"
+        # into the ETD's expected etd.files_embargoed = 'true', etd.toc_embargoed = 'true',
+        # etd.abstract_embargoed = 'true'
+        def translate_embargo_types(env)
+          return unless env.attributes[:embargo_type]
+          embargo_type = env.attributes.delete(:embargo_type)
+          env.attributes[:files_embargoed] = 'true' if embargo_type =~ /files_embargoed/
+          env.attributes[:toc_embargoed] = 'true' if embargo_type =~ /toc_embargoed/
+          env.attributes[:abstract_embargoed] = 'true' if embargo_type =~ /abstract_embargoed/
         end
     end
   end
