aef8616f1ede8a0cb3d949e4b0a0bb973f5f391e
Bess Sadler <bess@curationexperts.com>
2018-08-10 10:41:09 -0400

    Revert "Persist copyright questions"

--- app/javascript/formStore.js
+++ app/javascript/formStore.js
@@ -157,9 +157,6 @@ export var formStore = {
       return `/in_progress_etds/${this.ipeId}`
     }
   },
-  copyrights: 0,
-  permissions: 0,
-  patents: 0,
   copyrightQuestions: copyrightQuestions,
   committeeChairs: new ChairList(),
   committeeMembers: new MemberList(),
@@ -419,24 +416,8 @@ export var formStore = {
     if (this.supplementalFiles === undefined || this.supplementalFiles.length === 0) return
     return JSON.stringify(this.supplementalFiles)
   },
-  setCopyrights () {
-    this.copyrights = Number(!this.copyrights)
-  },
-  setPermissions () {
-    this.permissions = Number(!this.permissions)
-  },
-  setPatents () {
-    this.patents = Number(!this.patents)
-  },
-  getCopyrights () {
-    return `${this.copyrights}`
-  },
-  getPermissions () {
-    return `${this.permissions}`
-  },
-  getPatents () {
-    return `${this.patents}`
-  },
+
+
   getGraduationDate () {
     return this.savedData['graduation_date']
   },
