d32daf7f8a94dd95de58c059d4159440bc473d56
Jamie Little <jamie@jamielittle.org>
2018-08-10 09:44:59 -0500

    Don't allow changes to school after selected
    This commit changes the select
    dropdown for school so that a user
    can't change it after they have
    initially selected it.
    
    For #1562

--- app/javascript/test/School.spec.js
+++ app/javascript/test/School.spec.js
@@ -2,18 +2,30 @@
 /* global it */
 /* global expect */
 /* global jest */
-import { shallowMount } from '@vue/test-utils'
-import Language from 'School'
+import { mount } from '@vue/test-utils'
+import School from 'School'
 import axios from 'axios'
+import { formStore } from 'formStore'
+
 jest.mock('axios')
 
 describe('School.vue', () => {
   const resp = {data: [{'id': 'Candler School of Theology'}]}
   axios.get.mockResolvedValue(resp)
 
-  it('has the correct html', () => {
-    const wrapper = shallowMount(Language, {
+  it('is a a select box before saving', () => {
+    const wrapper = mount(School, {
     })
+    wrapper.vm.$data.sharedState.schools.options = [{"text":"Select a School","value":"","disabled":"disabled","selected":"selected"},{"text":"Candler School of Theology","value":"candler"},{"text":"Emory College","value":"emory"},{"text":"Laney Graduate School","value":"laney"},{"text":"Rollins School of Public Health","value":"rollins"}]
     expect(wrapper.html()).toContain(`<div><label for="school">School</label> <select id="school" aria-required="true" class="form-control"><option disabled="disabled" value="">`)
+  })
+
+  it('is non-editable text after saving', () => {
+    formStore.savedData.school = 'candler'
+
+    const wrapper = mount(School, {
+    })
+    wrapper.vm.$data.sharedState.schools.options = [{"text":"Select a School","value":"","disabled":"disabled","selected":"selected"},{"text":"Candler School of Theology","value":"candler"},{"text":"Emory College","value":"emory"},{"text":"Laney Graduate School","value":"laney"},{"text":"Rollins School of Public Health","value":"rollins"}]
+    expect(wrapper.html()).toContain(`<div><label for=\"school\">School</label> <!----> <div><b>Candler School of Theology</b></div></div>`)
   })
 })
