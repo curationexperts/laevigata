742c4fbe4c7d8e961e5114db2c38bc343642d2e9
Valerie Maher <val99erie@users.noreply.github.com>
2018-08-11 08:25:26 -0500

    Part of story #1445 - allow student to replace primary file (#1581)
    This code fixes a problem when the student had an existing primary PDF
    file, and then tried to add a supplemental file, there would be a 404
    error.
    
    The problem was caused by the fact that the 'uploaded_files' input on
    the form contained not just the newly-uploaded file record IDs, but also
    the ID of the previously saved primary PDF.  So when the controller
    received that extra ID, it would look for the corresponding
    Hyrax::UploadedFiles record (which doesn't exist).
    
    I added a conditional to the hidden 'uploaded_files' input that prevents
    previously-saved files from being added to the array.  Previously-saved
    files can be differentiated from newly-uploaded files by the deleteUrl,
    which contains the model name.

--- app/javascript/test/Files.spec.js
+++ app/javascript/test/Files.spec.js
@@ -2,7 +2,9 @@
 /* global it */
 /* global expect */
 import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import Files from 'Files'
+import { formStore } from 'formStore'
 
 describe('Files.vue', () => {
   global.Box = { FilePicker: function() {}}
@@ -19,4 +21,56 @@ describe('Files.vue', () => {
     
     expect(wrapper.findAll('label')).toHaveLength(2)
   })
+
+  it('when primary file is present, it returns primary file', () => {
+    const wrapper = shallowMount(Files, { })
+    const fakeFileData = { id: 'fake file object' }
+    formStore.files = [[fakeFileData]]
+
+    expect(wrapper.vm.getPrimaryFile()).toEqual(fakeFileData)
+  })
+
+  it('when no primary file exists, it returns undefined', () => {
+    const wrapper = shallowMount(Files, { })
+    formStore.files = []
+
+    expect(wrapper.vm.getPrimaryFile()).toEqual(undefined)
+  })
+
+  it('when the file is a Hyrax::UploadedFile, isUploadedFile() returns true', () => {
+    const wrapper = shallowMount(Files, { })
+    const fakeFileData = { deleteUrl: '/uploads/123' }
+
+    expect(wrapper.vm.isUploadedFile(fakeFileData)).toEqual(true)
+  })
+
+  it('when the file is a ::FileSet, isUploadedFile() returns false', () => {
+    const wrapper = shallowMount(Files, { })
+    const fakeFileData = { deleteUrl: '/concern/file_sets/456' }
+
+    expect(wrapper.vm.isUploadedFile(fakeFileData)).toEqual(false)
+  })
+
+  it('when the file is undefined, isUploadedFile() returns false', () => {
+    const wrapper = shallowMount(Files, { })
+
+    expect(wrapper.vm.isUploadedFile(undefined)).toEqual(false)
+  })
+
+  it('when the file is a Hyrax::UploadedFile, the uploaded_files input contains the record ID', () => {
+    const wrapper = mount(Files, { })
+    const fakeFileData = { deleteUrl: '/uploads/123', id: '123_my_super_unique_id' }
+    formStore.files = [[fakeFileData]]
+
+    expect(wrapper.html()).toContain('123_my_super_unique_id')
+  })
+
+  it('when the file is a ::FileSet, the uploaded_files doesn\'t contain the record ID', () => {
+    const wrapper = mount(Files, { })
+    const fakeFileData = { deleteUrl: '/concern/file_sets/123', id: '123_my_super_unique_id' }
+    formStore.files = [[fakeFileData]]
+
+    expect(wrapper.html()).not.toContain('123_my_super_unique_id')
+  })
 })
+
