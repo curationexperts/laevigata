be576a487e42f0170930487cfe6d5554cc8f30fd
Bess Sadler <bess@curationexperts.com>
2018-08-12 14:30:46 -0400

    Revert "Show school, department, and embargo length on edit screen" (#1609)

--- app/javascript/test/formStore.spec.js
+++ app/javascript/test/formStore.spec.js
@@ -4,10 +4,10 @@
 
 import { formStore } from '../formStore'
 
-formStore.getSavedOrSelectedSchool = jest.fn(() => {return 'Emory College'})
+formStore.getSavedOrSelectedSchool = jest.fn(() => {return 'emory'})
 describe('formStore', () => {
   it('returns the correct embargo length based on the selected school', () => {
-    formStore.setSelectedSchool('Emory College')
+    formStore.setSelectedSchool('emory')
     expect(formStore.getEmbargoLengths()).toEqual([{ value: 'None - open access immediately', selected: 'selected' },
     { value: '6 months' }, { value: '1 year' }, { value: '2 years' }])
   })
