6d6a6f3c0f575b94716c2493e42734285c567837
Bess Sadler <bess@curationexperts.com>
2018-08-12 22:07:52 -0400

    Remove formatting that won't be preserved
    Remove all formatting options except bold and
    italic. None of the others can be consistently
    preserved and displayed without significant
    development time.

--- app/javascript/quillToolbarOptions.js
+++ app/javascript/quillToolbarOptions.js
@@ -1,18 +1,9 @@
 export default function quillToolbarOptions () {
   return [
-    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-    ['blockquote', 'code-block'],
+    ['bold', 'italic'], // toggled buttons
 
-    [{ 'header': 1 }, { 'header': 2 }], // custom button values
-    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-    [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
-    [{ 'indent': '-1' }, { 'indent': '+1' }], // outdent/indent
-    [{ 'direction': 'rtl' }], // text direction
-
-    [{ 'size': ['small', false, 'large', 'huge'] }], // custom dropdown
-    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-
-    [{ 'align': [] }],
+//    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+//    [{ 'script': 'sub' }, { 'script': 'super' }], // superscript/subscript
 
     ['clean'] // remove formatting button
   ]
