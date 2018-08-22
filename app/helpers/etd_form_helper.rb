9caae607c313c4c4bd8693277b262f099303421d
val99erie <val99erie@gmail.com>
2018-08-22 10:02:24 -0500

    Turn off turbolinks for link to edit ETD record. Connected to #1731

--- app/helpers/etd_form_helper.rb
+++ app/helpers/etd_form_helper.rb
@@ -3,11 +3,11 @@
 module EtdFormHelper
   # If we're using the old UI, just use the regular path from Hyrax for editing Etd record.
   # If we're using the new UI, use the path for the InProgressEtdsController.
-  def etd_edit_link(presenter)
+  def etd_edit_link(presenter, link_text='Edit')
     if Rails.application.config_for('new_ui').fetch('enabled', false)
-      link_to 'Edit', main_app.new_in_progress_etd_path(etd_id: presenter.id), class: 'btn btn-default', data: { turbolinks: false }
+      link_to link_text, main_app.new_in_progress_etd_path(etd_id: presenter.id), class: 'btn btn-default', data: { turbolinks: false }
     else
-      link_to 'Edit', edit_polymorphic_path([main_app, presenter]), class: 'btn btn-default'
+      link_to link_text, edit_polymorphic_path([main_app, presenter]), class: 'btn btn-default'
     end
   end
 end
