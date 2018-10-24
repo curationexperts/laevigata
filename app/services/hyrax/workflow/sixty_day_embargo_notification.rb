1000aafc0101b48e34b796cd6f437fe52f3cc8c9
devanshu-m <devanshu_m@gwmail.gwu.edu>
2018-10-24 14:09:12 -0400

    Applies text changes to embargo notifications and My Files page
    Adds message that asks users to wait while their primary PDF gets uploaded under the My Files tab. Also applies minor text changes to embargo notifications.

--- app/services/hyrax/workflow/sixty_day_embargo_notification.rb
+++ app/services/hyrax/workflow/sixty_day_embargo_notification.rb
@@ -13,13 +13,14 @@ module Hyrax
         "Dear #{@user.display_name},\n\n" \
         "The embargo on your dissertation or thesis, #{title}, will expire in sixty days.\n" \
         "Your dissertation or thesis can be accessed in the Emory University ETD Repository at: #{link_to document_url, document_url} \n\n" \
-        "If you need your record to remain restricted/embargoed, please contact etd-help@listserv.cc.emory.edu<mailto:etd-help@listserv.cc.emory.edu> with your request. \n\n" \
-        "Please do not reply directy to this email.\n\n" \
+        "If you need your record to remain restricted/embargoed, please contact your school administrators " \
+        "located here: #{link_to 'http://sco.library.emory.edu/etds/contact.html', 'http://sco.library.emory.edu/etds/contact.html'} with your request. \n\n" \
+        "Please do not reply directly to this email.\n\n" \
 
         "FOR AUTHORS WHO ALSO SUBMITTED TO PROQUEST:\n" \
         "To extend your access restriction/embargo in the ProQuest database, you " \
         "will need to contact ProQuest's Author Relations department to request " \
-        "they extend your embargo. You can reach them at 1-800-521-0600 ext. 77020 " \
+        "an extended access restriction/embargo in their database. You can reach them at 1-800-521-0600 ext. 77020 " \
         "or via email at disspub@proquest.com<mailto:disspub@proquest.com>."
       end
     end
