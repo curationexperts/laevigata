2d4cdd9ae5d97db6be7a0e118c6fedf6e5259a08
Bess Sadler <bess@curationexperts.com>
2019-02-14 15:43:53 -0500

    Fix truncated embargo notification message
    This fixes a bug where the embargo notification message was being
    truncated.

--- app/services/hyrax/workflow/seven_day_embargo_notification.rb
+++ app/services/hyrax/workflow/seven_day_embargo_notification.rb
@@ -10,18 +10,21 @@ module Hyrax
       end
 
       def message
-        "Dear #{@user.display_name},\n\n" \
-        "The embargo on your dissertation or thesis, #{title}, will expire in seven days.\n" \
-        "Your dissertation or thesis can be accessed in the Emory University ETD Repository at: #{link_to document_url, document_url} \n\n" \
-        "If you need your record to remain restricted/embargoed, please contact your school administrators " \
-        "located here: #{link_to 'http://sco.library.emory.edu/etds/contact.html', 'http://sco.library.emory.edu/etds/contact.html'} with your request. \n\n" \
-        "Please do not reply directly to this email.\n\n" \
+        %(
+        Dear #{@user.display_name},\n\n
+        The embargo on your dissertation or thesis, #{title}, will expire in seven days.
+        Your dissertation or thesis can be accessed in the Emory University ETD Repository at: #{link_to document_url, document_url}
 
-        "FOR AUTHORS WHO ALSO SUBMITTED TO PROQUEST:\n" \
-        "To extend your access restriction/embargo in the ProQuest database, you " \
-        "will need to contact ProQuest's Author Relations department to request " \
-        "an extended access restriction/embargo in their database. You can reach them at 1-800-521-0600 ext. 77020 " \
-        "or via email at disspub@proquest.com<mailto:disspub@proquest.com>."
+        If you need your record to remain restricted/embargoed, please contact your school administrators
+        located here: #{link_to 'http://sco.library.emory.edu/etds/contact.html', 'http://sco.library.emory.edu/etds/contact.html'} with your request. \n\n
+        Please do not reply directly to this email.
+
+        FOR AUTHORS WHO ALSO SUBMITTED TO PROQUEST:
+        To extend your access restriction/embargo in the ProQuest database, you
+        will need to contact ProQuest's Author Relations department to request
+        an extended access restriction/embargo in their database. You can reach them at 1-800-521-0600 ext. 77020
+        or via email at disspub@proquest.com<mailto:disspub@proquest.com>.
+      )
       end
     end
   end
