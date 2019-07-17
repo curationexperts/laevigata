2848bd885a946aa8422d0cc8478218f0a2cf0b55
Jamie Little <jamie@jamielittle.org>
2019-07-17 16:34:03 -0500

    Format date in workflow admin page
    This adds a helper to format the
    date in the workflow admin page so that
    is displays the time in EST and using AM/PM.
    
    Connected to #1939

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationHelper, type: :helper do
+  let(:time) { '2019-07-17 17:08:08 -0400' }
+  let(:est_time) { '07/17/19 05.07 PM' }
+  describe '#formatted_date' do
+    it 'returns an EST date/time with AM and PM' do
+      expect(helper.formatted_date(time: time)). to eq(est_time)
+    end
+  end
+end
