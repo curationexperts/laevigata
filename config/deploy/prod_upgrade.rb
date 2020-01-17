c10eb817dd7dea2904a2b9c4cc4df67f9dc918c0
Max Kadel <max@curationexperts.com>
2020-01-17 15:41:34 -0500

    Standardize capistrano deploy target names

--- config/deploy/prod_upgrade.rb
+++
@@ -1,4 +0,0 @@
-# deploys to Emory production upgrade machine
-set :stage, :prod_upgrade
-set :rails_env, 'production'
-server 'upgrade-etd.emory.edu', user: 'deploy', roles: [:web, :app, :db]
--- config/deploy/staging_upgrade.rb
+++
@@ -1,4 +0,0 @@
-# deploys to Emory staging-upgrade machine
-set :stage, :staging_upgrade
-set :rails_env, 'production'
-server 'staging-upgrade-etd.emory.edu', user: 'deploy', roles: [:web, :app, :db]
