4e98754e2ce0bdf2b8db4e9f41b3da231ebe99ca
Rachel Kadel <rachel.kadel@curationexperts.com>
2020-10-07 17:06:02 -0400

    Set up basic framework for deployment/smoke test (#2062)
    * Set up basic framework for deployment/smoke test
    
    Usage: Set environment variables for HTTP_USERNAME, HTTP_PASSWORD, 
    ETD_HOST.  Then run `rspec smoke_spec`.  At present, only looks for 
    existence of a front page with the "Emory Theses and Dissertations" 
    title.
    
    * Style check

---
+++ smoke_spec/deploy_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+require_relative './spec_helper'
+
+username = ENV['HTTP_USERNAME'] || 'test'
+password = ENV['HTTP_PASSWORD'] || 'test'
+hostname = ENV['ETD_HOST']
+# use this SSLContext to use https URLs without verifying certificates
+# @ssl_context = OpenSSL::SSL::SSLContext.new
+# @ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+ssl_context = OpenSSL::SSL::SSLContext.new
+ssl_context.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+RSpec.describe "The ETD server at #{hostname}", type: :feature do
+  let(:uri) { "https://#{username}:#{password}@#{hostname}" }
+  it "loads the front page" do
+    visit uri
+    expect(page).to have_content("Emory Theses and Dissertations")
+  end
+end
