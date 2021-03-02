e512053caf343689689f7bfdd94e1e99ce625e65
Max Kadel <max@curationexperts.com>
2021-03-02 14:44:29 -0500

    Include more formatters

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,10 @@
 unless ENV['NO_COVERAGE'] == 'true'
   require 'simplecov'
   require 'coveralls'
-  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+    SimpleCov::Formatter::HTMLFormatter,
+    Coveralls::SimpleCov::Formatter
+  ])
   SimpleCov.start 'rails' do
     # Can filter out files from coverage reports, example below.
     # add_filter 'app/controllers/metadata_samples_controller.rb'
