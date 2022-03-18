4ac408c98624745e9961aae67a116021a7e09427
McClain Looney <mlooney@users.noreply.github.com>
2022-03-18 13:17:02 -0500

    Silences redis deprecations (#2224)
    Co-authored-by: McClain Looney <mcclain@curationexperts.com>

--- config/initializers/redis_config.rb
+++ config/initializers/redis_config.rb
@@ -1,3 +1,4 @@
 require 'redis'
 config = YAML.safe_load(ERB.new(IO.read(Rails.root.join('config', 'redis.yml'))).result)[Rails.env].with_indifferent_access
+Redis.silence_deprecations
 Redis.current = Redis.new(config.merge(thread_safe: true))
