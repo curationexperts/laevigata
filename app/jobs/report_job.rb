563e9b051a9730c1c2ea0ecbb288f2c358e329e9
McClain Looney <mcclain@curationexperts.com>
2022-03-31 02:03:07 -0400

    Finds only graduated etds, handles multivalue fields

--- app/jobs/report_job.rb
+++ app/jobs/report_job.rb
@@ -1,24 +1,29 @@
 require 'csv'
 class ReportJob < ApplicationJob
   def perform
-    etds = Etd.all
+    etds = Etd.where("graduation_date_tesim:*")
+    headers = %w[id creator title school department degree submitting_type
+                 language subfield research_field keyword committee_chair committee_members
+                 post_graduation_email graduation_year degree_awarded graduation_date partnering_agency
+                 date_created date_uploaded abstract_length abstract]
 
-    puts etds.count
-    headers = %w[id creator title school department degree submitting_type language subfield research_field keyword committee_chair committee_members post_graduation_email graduation_year degree_awarded graduation_date partnering_agency date_created date_uploaded abstract_length abstract]
-
-    CSV.open(Rails.root.join("public/report.csv").to_s, "wb", write_headers: true, headers: headers) do |csv|
-      etds.each.take(10).each do |etd|
-        row = headers.map do |x| 
+    CSV.open(Rails.root.join("public", "report.csv").to_s, "wb", write_headers: true, headers: headers) do |csv|
+      etds.each do |etd|
+        row = headers.map do |x|
           case x
           when "graduation_year"
             etd.graduation_date
           when "abstract_length"
             etd.abstract.first.length
           else
-            v = etd.send(x)
-            v.is_a?(ActiveTriples::Relation) ? v.first : v
+            k = etd.send(x)
+            if k.is_a?(ActiveTriples::Relation)
+              k.map { k.first }.join('; ')
+            else
+              k
+            end
           end
-        end 
+        end
 
         csv << row
       end
