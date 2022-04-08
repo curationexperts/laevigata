4a116306cf02b8a7a327209e21f95a487bf2c804
McClain Looney <mlooney@users.noreply.github.com>
2022-04-08 11:15:30 -0500

    Adds timestamp to private files (#2240)
    * Adds timestamp to private files
    
    * Rubocop
    
    Co-authored-by: McClain Looney <mcclain@curationexperts.com>

--- app/controllers/private_file_controller.rb
+++ app/controllers/private_file_controller.rb
@@ -3,7 +3,9 @@ class PrivateFileController < ApplicationController
 
   # sends a file from the private filesystem
   def download
-    send_file(Rails.root.join("private", [params[:file_name], '.', params[:format]].join))
+    filename = [params[:file_name], '.', params[:format]].join
+    base, suffix = filename.split(".")
+    send_file(Rails.root.join("private", filename), filename: "#{base}_#{Time.zone.now.strftime('%Y%m%d')}.#{suffix}")
   end
 
   private
