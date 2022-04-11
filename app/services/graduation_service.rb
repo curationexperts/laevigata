75765e1bd786156a679c38ab2526305f513acd1c
Mark Bussey <mark@curationexperts.com>
2022-04-11 12:08:14 -0500

    Refactor the GraduationService to safely handle repeated calls
    Switch from using class methods to instance methods so multiple
    overlapping calls to the service won't clobber class-level attributes.

--- app/services/graduation_service.rb
+++ app/services/graduation_service.rb
@@ -1,5 +1,5 @@
-# An automated service that:
-# 1. Checks the repository for works in the `approved` workflow state but which have no `degree_awarded` value
+# A service that:
+# 1. Checks the repository for works in the `approved` workflow state
 # 2. For each of these works, query the registrar data to see if the student has graduated
 # 3. If so, call GraduationJob for the given work and the graduation_date returned by registrar data
 # @param [String] The path to the data JSON file. Expects location of registrar data to be set in REGISTRAR_DATA_PATH, e.g., in .env.production
@@ -7,34 +7,37 @@
 #  GraduationService.run
 class GraduationService
   def self.run(registrar_data_path = ENV['REGISTRAR_DATA_PATH'])
+    new(registrar_data_path).run
+  end
+
+  # Load the provided file into a JSON object for processing
+  def initialize(registrar_data_path)
+    raise "Cannot find registrar data at: #{registrar_data_path || 'no path provided'}" unless File.file?(registrar_data_path)
     Rails.logger.warn("Graduation service: Running graduation service with file #{registrar_data_path}")
-    raise "Cannot find registrar data" unless registrar_data_path && File.file?(registrar_data_path)
-    Rails.logger.warn("Graduation service: found registrar data")
-    GraduationService.load_data(registrar_data_path)
-    GraduationService.graduation_eligible_works.each do |work|
-      degree_awarded_date = GraduationService.check_degree_status(work)
+    @registrar_data = JSON.parse(File.read(registrar_data_path))
+  end
+
+  # Find all ETDs that have been 'approved' but not yet 'published'
+  # Match them to graduation records and check whether a degree has been awarded yet
+  def run
+    return unless @registrar_data
+    graduation_eligible_works.each do |work|
+      degree_awarded_date = check_degree_status(work)
       if degree_awarded_date
         Rails.logger.warn "Graduation service: Awarding degree for ETD #{work['id']} as of #{degree_awarded_date}"
         GraduationJob.perform_now(work['id'], degree_awarded_date.to_s)
       end
     end
-    remove_instance_variable(:@registrar_data)
-  end
-
-  # Load the Registrar's data from the JSON file.
-  # @param [String] The path to the data JSON file.
-  def self.load_data(path_to_data)
-    @registrar_data = JSON.parse(File.read(path_to_data))
   end
 
   # Find all Etds in the 'approved' workflow state that are eligible for graduation
-  # @return [Array<SolrDocument>] An Array of SolrDocuments indexed for the matching ETDs
-  def self.graduation_eligible_works
+  # @return [Array<Hash>] An Array of Hashes representing the ETDs indexed to Solr
+  def graduation_eligible_works
     eligible_works = []
     # Use #search_in_batches to avoid timeouts in the case where there are a large number of ETDs
     # that have been approved and are pending graduation (i.e. publication)
     Etd.search_in_batches({ workflow_state_name_ssim: 'approved' }, batch_size: 50) do |batch|
-      eligible_works.concat(batch.to_a)
+      eligible_works.concat(batch)
     end
 
     Rails.logger.warn "Graduation service: There were #{eligible_works.count} ETDs eligible for graduation"
@@ -42,9 +45,9 @@ class GraduationService
   end
 
   # Check the degree status for the given work. If the degree has been awarded, return the relevant date.
-  # @param [SolrDocument] work - the SolrDocument indexed for the corresponding ETD
-  # @return [Date] the date the degree was awarded, otherwise false
-  def self.check_degree_status(work)
+  # @param [Hash] work - the Solr document Hash for the corresponding ETD
+  # @return [Date] the date the degree was awarded, otherwise nil
+  def check_degree_status(work)
     return Time.zone.today.strftime('%Y-%m-%d') if Rails.env.development? || ENV['FAKE_DATA'] # Otherwise it will never find registrar data for our fake users
     # Find all records by this PPID
     records = @registrar_data.select { |_k, v| v['public person id'] == work['depositor_ssim'].first }
@@ -78,7 +81,7 @@ class GraduationService
 
   # If there is more than one record matching the student, find the one that matches the degree code
   # of the etd. If that record has it's degree awarded, graduate the etd.
-  def self.disambiguate_registrar(work, records)
+  def disambiguate_registrar(work, records)
     degree_code_matches = records.select { |_key, value| value['degree code'] == work_degree(work['degree_tesim'].first) }
     if degree_code_matches.size == 1 && degree_code_matches.first[1]['degree status descr'] == 'Awarded'
       return degree_code_matches.first[1]
@@ -86,7 +89,7 @@ class GraduationService
     nil
   end
 
-  def self.work_degree(work_degree)
+  def work_degree(work_degree)
     # This hash will map degree codes from laevigata to degrees in @registrar_data
     degrees = { "Th.D." => "THD", "Ph.D." => "PHD", "DMin" => "DM", "D.N.P." => "DNP",
                 "M.A." => "MA", "M.S." => "MS", "M.Div." => "MDV", "M.T.S." => "MTS",
