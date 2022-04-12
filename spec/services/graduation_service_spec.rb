e752ef9a3bf48e1a13790c52d186fc51ea5f0607
Mark Bussey <mark@curationexperts.com>
2022-04-11 23:30:52 -0500

    Add GraduationService reporting
    * Refactors code to more easily compute approved and published ETD counts.
    * Corrects rake task to send log data to console and application logfile
    * Speeds up service by only scanning Registrar data once, rather than once
      per approved ETD

--- spec/services/graduation_service_spec.rb
+++ spec/services/graduation_service_spec.rb
@@ -8,7 +8,7 @@ describe GraduationService, :clean do
   let(:double_degree_user) { FactoryBot.create(:double_degree_user) }
   let(:approving_user) { User.where(uid: "candleradmin").first }
   let(:w) { WorkflowSetup.new("#{fixture_path}/config/emory/superusers.yml", "#{fixture_path}/config/emory/candler_admin_sets.yml", "/dev/null") }
-  let(:graduated_etd) { FactoryBot.actor_create(:sample_data, user: graduated_user) }
+  let(:graduated_etd) { FactoryBot.actor_create(:sample_data, degree: ["B.S."], user: graduated_user) }
   let(:nongraduated_etd) { FactoryBot.actor_create(:sample_data, user: nongraduated_user) }
   # ETD for user that is graduated with one degree but is pursuing another degree with Emory
   let(:double_degree_etd) { FactoryBot.actor_create(:sample_data, degree: ["M.Div."], user: double_degree_user) }
@@ -30,23 +30,38 @@ describe GraduationService, :clean do
   end
 
   describe "#run" do
-    it "finds all works in an approved state that do not yet have a degree_awarded value" do
-      expect(graduated_etd.to_sipity_entity.reload.workflow_state_name).to eq "approved"
+    it "publishes approved & graduated etds", :aggregate_failures do
+      expect(graduated_etd.to_sipity_entity.workflow_state_name).to eq "approved"
+      expect(nongraduated_etd.to_sipity_entity.workflow_state_name).to eq "approved"
+      expect(double_degree_etd.to_sipity_entity.workflow_state_name).to eq "approved"
+
+      described_class.new('./spec/fixtures/registrar_sample.json').run
+
+      # only publishes approved etds with matching registrar data
+      expect(graduated_etd.to_sipity_entity.reload.workflow_state_name).to eq "published"
       expect(nongraduated_etd.to_sipity_entity.reload.workflow_state_name).to eq "approved"
-      expect(double_degree_etd.to_sipity_entity.reload.workflow_state_name).to eq "approved"
+      expect(double_degree_etd.to_sipity_entity.reload.workflow_state_name).to eq "published"
+
+      # Persists graduation date
+      expect(graduated_etd.reload.degree_awarded).to eq Date.new(2017, 5, 18)
+      expect(nongraduated_etd.reload.degree_awarded).to eq nil
+      expect(double_degree_etd.reload.degree_awarded).to eq Date.new(2018, 1, 12)
+    end
+    it "finds approved etds" do
       grad_service = described_class.new('./spec/fixtures/registrar_sample.json')
       expect(grad_service.graduation_eligible_works.map { |doc| doc['id'] }).to contain_exactly(graduated_etd.id, nongraduated_etd.id, double_degree_etd.id)
     end
-    it "checks the graduation status for a given work" do
+  end
+  describe "#status" do
+    it "provides statistics for a run", :aggregate_failures do
       grad_service = described_class.new('./spec/fixtures/registrar_sample.json')
-      expect(grad_service.check_degree_status(graduated_etd.to_solr)).to eq Date.new(2017, 5, 18)
-      expect(grad_service.check_degree_status(nongraduated_etd.to_solr)).to eq nil
-      expect(grad_service.check_degree_status(double_degree_etd.to_solr)).to eq Date.new(2018, 1, 12)
-    end
-    it "checks for new graduates" do
-      allow(GraduationJob).to receive(:perform_now)
-      described_class.run('./spec/fixtures/registrar_sample.json')
-      expect(GraduationJob).to have_received(:perform_now).twice
+
+      expect(grad_service.status.overall).to eq 'Not run'
+      grad_service.run
+      expect(grad_service.status.overall).to eq 'Completed'
+
+      expect(grad_service.status['degree_eligible_etds']).to eq 3
+      expect(grad_service.status['newly_published_etds']).to eq 2
     end
   end
 end
