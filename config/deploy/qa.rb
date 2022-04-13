fcaef7d5962c39fbf32cd9c336a5d33e95807da3
Mark Bussey <mark@curationexperts.com>
2022-04-13 14:03:11 -0500

    Remove legacy QA approver setup
    This can now be handled via admin_sets.yaml file on the QA server.

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -2,10 +2,3 @@
 set :stage, :qa
 set :rails_env, 'production'
 server 'qa-etd.library.emory.edu', user: 'deploy', roles: [:web, :app, :db]
-namespace :deploy do
-  after :finishing, :qa_approver_setup do
-    on roles(:app) do
-      execute "cd #{deploy_to}/current; /usr/bin/env bundle exec rake emory:qa_approver_setup RAILS_ENV=production"
-    end
-  end
-end
