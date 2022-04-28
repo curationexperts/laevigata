a3b8912a13221861d9bac9f8023e3b56826c0c32
Mark Bussey <mark@curationexperts.com>
2022-04-28 16:35:22 -0500

    Refactor to remove instance variables from ProquestJob

--- app/jobs/proquest_job.rb
+++ app/jobs/proquest_job.rb
@@ -14,17 +14,17 @@ class ProquestJob < ActiveJob::Base
 
   # @param [String] work_id - the id of the work object
   def perform(work_id, transmit: true, cleanup: true, retransmit: false)
-    @work = Etd.find(work_id)
-    return unless ProquestJob.submit_to_proquest?(@work, retransmit)
+    work = Etd.find(work_id)
+    return unless ProquestJob.submit_to_proquest?(work, retransmit)
     Rails.logger.info "ETD #{work_id} beginning submission to ProQuest"
     # 1. Create a directory. Done. See config/environments
     # 2. Write XML file there Done.
     # 3. Write PDF and supplementary files there. Done.
     # 4. Zip the directory. Done. Tasks 1 - 4 encapsulated in export_zipped_proquest_package
-    @upload_file = @work.export_zipped_proquest_package
+    upload_file = work.export_zipped_proquest_package
     # 5. Transmit the zip to ProQuest
-    transmit_file if transmit
-    cleanup_file if cleanup
+    transmit_file(upload_file, work.upload_file_id) if transmit
+    cleanup_file(upload_file) if cleanup
     ProquestCompletionJob.perform_later(work_id)
     Rails.logger.info "ETD #{work_id} finished submission to ProQuest"
   end
@@ -50,15 +50,15 @@ class ProquestJob < ActiveJob::Base
   end
 
   # Transmit the exported file to ProQuest SFTP server
-  def transmit_file
+  def transmit_file(upload_file, file_id)
     Net::SFTP.start(ENV['PROQUEST_SFTP_HOST'], ENV['PROQUEST_SFTP_USER'], password: ENV['PROQUEST_SFTP_PASSWORD']) do |sftp|
-      Rails.logger.debug "Uploading #{@upload_file} to ProQuest"
-      sftp.upload!(@upload_file, "#{@work.upload_file_id}.zip")
+      Rails.logger.debug "Uploading #{upload_file} to ProQuest"
+      sftp.upload!(upload_file, "#{file_id}.zip")
     end
   end
 
   # Delete the transmitted file
-  def cleanup_file
-    File.delete(@upload_file)
+  def cleanup_file(upload_file)
+    File.delete(upload_file)
   end
 end
