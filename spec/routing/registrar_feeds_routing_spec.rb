c213733d64a95de1990287daf6fabaed9cdfb60b
Mark Bussey <mark@curationexperts.com>
2023-03-08 14:48:05 -0600

    Restrict Registrar Feed access to administrators only
    This change adds routing restrictions that limit access to Registrar
    data.  Only full administrators ("super-admins") should be able to
    post and view registrar data to the system.

--- spec/routing/registrar_feeds_routing_spec.rb
+++ spec/routing/registrar_feeds_routing_spec.rb
@@ -1,38 +1,55 @@
 require "rails_helper"
 
 RSpec.describe RegistrarFeedsController, type: :routing do
-  describe "routing" do
+  describe "admin routing" do
+    before { allow(UserIsAdmin).to receive(:matches?).and_return(true) }
+
     it "routes to #index" do
-      expect(:get => "/registrar_feeds").to route_to("registrar_feeds#index")
+      expect(:get => "/admin/registrar_feeds").to route_to("registrar_feeds#index")
     end
 
     it "routes to #new" do
-      expect(:get => "/registrar_feeds/new").to route_to("registrar_feeds#new")
+      expect(:get => "/admin/registrar_feeds/new").to route_to("registrar_feeds#new")
     end
 
     it "routes to #show" do
-      expect(:get => "/registrar_feeds/1").to route_to("registrar_feeds#show", :id => "1")
+      expect(:get => "/admin/registrar_feeds/1").to route_to("registrar_feeds#show", :id => "1")
     end
-
-    it "routes to #edit" do
-      expect(:get => "/registrar_feeds/1/edit").to route_to("registrar_feeds#edit", :id => "1")
-    end
-
 
     it "routes to #create" do
-      expect(:post => "/registrar_feeds").to route_to("registrar_feeds#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(:put => "/registrar_feeds/1").to route_to("registrar_feeds#update", :id => "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(:patch => "/registrar_feeds/1").to route_to("registrar_feeds#update", :id => "1")
+      expect(:post => "/admin/registrar_feeds").to route_to("registrar_feeds#create")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/registrar_feeds/1").to route_to("registrar_feeds#destroy", :id => "1")
+      expect(:delete => "/admin/registrar_feeds/1").to route_to("registrar_feeds#destroy", :id => "1")
+    end
+  end
+
+  # Registrar Feed records should not be user-modifiable after initial upload
+  # even by administrators
+  describe 'disabled actions' do
+    before { allow(UserIsAdmin).to receive(:matches?).and_return(true) }
+
+    it "cannot route to #edit" do
+      expect(:get => "/admin/registrar_feeds/1/edit").to route_to("pages#error_404", path: "admin/registrar_feeds/1/edit")
+    end
+
+    it "cannot route to #update via PUT" do
+      expect(:put => "/admin/registrar_feeds/1").to route_to("pages#error_404", path: "admin/registrar_feeds/1")
+    end
+
+    it "cannot route to #update via PATCH" do
+      expect(:patch => "/admin/registrar_feeds/1").to route_to("pages#error_404", path: "admin/registrar_feeds/1")
+    end
+  end
+
+  # Registar Feed data should not be accessible to non-admins
+  describe "for non-admins" do
+    before { allow(UserIsAdmin).to receive(:matches?).and_return(false) }
+
+    # NOTE: we only need to test one action because all actions are governed by the same routing constraint
+    it "restricts #index" do
+      expect(:get => "/admin/registrar_feeds").to route_to("pages#error_404", path: "admin/registrar_feeds")
     end
   end
 end
