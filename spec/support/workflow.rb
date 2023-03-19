5d3b33874a7da7c74384d20039fd7e197ef1d7a4
Mark Bussey <mark@curationexperts.com>
2023-03-19 17:43:35 -0500

    Remove unused code
    The method removed here is not called by any other code in the
    test suite.

--- spec/support/workflow.rb
+++ spec/support/workflow.rb
@@ -4,10 +4,6 @@ module Workflow
     sipity_workflow_action = PowerConverter.convert_to_sipity_action(action_name, scope: subject.entity.workflow) { nil }
     Hyrax::Workflow::WorkflowActionService.run(subject: subject, action: sipity_workflow_action, comment: nil)
   end
-
-  def approve_etd(etd, approving_user)
-    change_workflow_status(etd, 'approve', approving_user)
-  end
 end
 
 RSpec.configure do |config|
