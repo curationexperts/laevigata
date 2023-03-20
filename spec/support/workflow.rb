deee29d76c5387a9fadaae49fa8edf08bf22b9b7
Mark Bussey <mark@curationexperts.com>
2023-03-20 13:20:48 -0500

    Clean up Laney workflow test
    * Consolidate include expecatations to speed up test
    * Stub out jobs unrelated to features being test to speed up test
    * Use change_workflow_status method to improve test legibility

--- spec/support/workflow.rb
+++ spec/support/workflow.rb
@@ -1,8 +1,8 @@
 module Workflow
-  def change_workflow_status(etd, action_name, user)
+  def change_workflow_status(etd, action_name, user, comment = nil)
     subject = Hyrax::WorkflowActionInfo.new(etd, user)
     sipity_workflow_action = PowerConverter.convert_to_sipity_action(action_name, scope: subject.entity.workflow) { nil }
-    Hyrax::Workflow::WorkflowActionService.run(subject: subject, action: sipity_workflow_action, comment: nil)
+    Hyrax::Workflow::WorkflowActionService.run(subject: subject, action: sipity_workflow_action, comment: comment)
   end
 end
 
