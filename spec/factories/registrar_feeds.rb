adb78f6d1a0670dbb43434ffe0688cc54e6b4af7
Mark Bussey <mark@curationexperts.com>
2023-06-09 15:17:01 -0500

    Allow GraduationService to process CSV data
    The registrar provides data in CSV format and folks have been
    converting it to JSON for processing by the application.
    
    Rather than converting the data to JSON and then parsing it back
    to a hash from JSON, this change enables the application to
    process the CSV file directly from the registrar.
    
    JSON support is still provided for backwards compatibility. This
    also allows the change to be deployed to production independently of
    cutting over the data format in the registrar workflow.

--- spec/factories/registrar_feeds.rb
+++ spec/factories/registrar_feeds.rb
@@ -1,16 +1,23 @@
 FactoryBot.define do
-  registrar_sample = Rack::Test::UploadedFile.new(
+  registrar_sample_json = Rack::Test::UploadedFile.new(
     Rails.root.join('spec', 'fixtures', 'registrar_feeds', 'registrar_sample.json'), 'application/json'
+  )
+  registrar_sample_csv = Rack::Test::UploadedFile.new(
+    Rails.root.join('spec', 'fixtures', 'registrar_feeds', 'registrar_sample.csv'), 'text/csv'
   )
   sample_report = Rack::Test::UploadedFile.new(
     Rails.root.join('spec', 'fixtures', 'registrar_feeds', 'graduation_report.csv'), 'text/csv'
   )
 
   factory :registrar_feed do
-    graduation_records { registrar_sample }
+    graduation_records { registrar_sample_csv }
     approved_etds { 1 }
     graduated_etds { 0 }
     published_etds { 2 }
+
+    factory :json_registrar_feed do
+      graduation_records { registrar_sample_json }
+    end
 
     factory :completed_registrar_feed do
       report { sample_report }
