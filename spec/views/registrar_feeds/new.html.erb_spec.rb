72e241b759caf66cc241033daa82384c73dbdae6
Mark Bussey <mark@curationexperts.com>
2023-06-13 15:27:42 -0500

    Allow the new registrar feed form to accept both CSV and JSON

--- spec/views/registrar_feeds/new.html.erb_spec.rb
+++ spec/views/registrar_feeds/new.html.erb_spec.rb
@@ -8,7 +8,9 @@ RSpec.describe "registrar_feeds/new", type: :view do
   it "renders new registrar_feed form", :aggregate_failures do
     render
     expect(rendered).to have_selector("form")
-    expect(rendered).to have_field(name: "registrar_feed[graduation_records]")
+    # expect(rendered).to have_field(name: "registrar_feed[graduation_records]")
+    # Instead, check that the file input allows the expected file types
+    expect(rendered).to have_xpath('//input[@id="registrar_feed_graduation_records" and contains(@accept, "application/json") and contains(@accept, "text/csv")]')
     expect(rendered).to have_button("commit", type: "submit")
   end
 end
