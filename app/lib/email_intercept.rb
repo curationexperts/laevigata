be2469e8ee1aaa58303a8137a66343c04ddff34d
Mark Bussey <mark@curationexperts.com>
2024-09-13 16:56:22 -0700

    [REFACTOR] Remove dead code - do_not_send list
    **ISSUE**
    The EmailIntercept class is not currently fully covered by the test
    suite. In particular, the config file loading safety check is not
    tested. However, we are not actively maintaining this list and prefer
    deactivating accounts instead. The only remaining function of this
    code is to prevent bounces from the dummy account.
    
    **RESOLUTION**
    Eliminate the configuration file since it is no longer in use.
    Update the documentation appropriately.

--- app/lib/email_intercept.rb
+++ app/lib/email_intercept.rb
@@ -1,15 +1,11 @@
 require 'yaml'
 
 class EmailIntercept
+  # Filter email prior to sending
+  # Currently only filters out messages to dummy accounts to prevent bounces
   def self.delivering_email(message)
-    do_not_send_file = Rails.root.join('config', 'emory', 'do_not_send.yml')
-    unless File.file? do_not_send_file
-      Rails.logger.error "Missing config file: #{do_not_send_file}"
-      return
-    end
-    do_not_send = YAML.load_file(do_not_send_file)
     message.to.each do |recipient|
-      message.perform_deliveries = false if do_not_send.include? recipient
+      message.perform_deliveries = false if recipient == 'tezprox@emory.edu'
     end
   end
 end
