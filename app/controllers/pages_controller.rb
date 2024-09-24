dd1eea8a9598737736b2fdd585473ca75b9f0c94
Mark Bussey <mark@curationexperts.com>
2024-09-23 21:48:30 -0500

    [REFACTOR] Clean up 404 Error page
    **RATIONALE**
    Rather than giving a dismissable flash message, we want to just
    give a simple page with a clear call-to-action for 404 errors.
    
    This change also updates the related test to actually trigger the
    Blacklight::Exceptions::RecordNotFound handler.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@
 
 class PagesController < ApplicationController
   def error_404
-    flash[:alert] = 'The page you are looking for does not exist.'
     render status: 404
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,9 +3,10 @@
 require 'rails_helper'
 
 RSpec.describe PagesController do
-  it 'has a flash alert & 404' do
+  it 'has a expected status and links', :aggregate_failures do
     get :error_404
-    expect(flash[:alert]).not_to be(nil)
+    expect(flash).to be_empty
     expect(response).to have_http_status(404)
+    expect(response).to render_template 'pages/error_404'
   end
 end
